6d6d1f3613d8b2c5d98b0ad90e5a9537691c3eaf
resteqs <65457699+resteqs@users.noreply.github.com>
2021-07-09 10:22:52 +0200

    v.20210709.1 vorlagen + .gitkeep

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // eslint-disable-next-line import/no-named-as-default
 import home from "./home";
 
-import sdg12 from "./SDG/SDG12/sdg12";
-
-// eslint-disable-next-line import/no-named-as-default
-import sdg13 from "./SDG/SDG13/sdg13";
-
 // eslint-disable-next-line import/no-duplicates
 // eslint-disable-next-line import/no-named-as-default
 import dictionary from "./SDG/dictionary/dictionary";
@@ -18,54 +13,54 @@ import co2 from "./SDG/dictionary/co2";
 import dextrose from "./SDG/dictionary/dextrose";
 import methane from "./SDG/dictionary/methane";
 import photosynthese from "./SDG/dictionary/photsynthese";
-
+import infotextvorlage from "./SDG/Vorlagen/infotext";
 
 import "./styles/App.css";
 import "./styles/sdg13.css";
 import "./styles/sdg14.css";
 import help from "./help";
 
-import { play13 }  from "./SDG/SDG13/U1/play";
+import { play13 }  from "./SDG/SDG13/Spiel/play";
 
-import akw from "./SDG/SDG13/U1/information/akw";
-import cow from "./SDG/SDG13/U1/information/cow";
-import bicycle from "./SDG/SDG13/U1/information/bicycle";
-import windTurbine from "./SDG/SDG13/U1/information/windTurbine";
-import tractor from "./SDG/SDG13/U1/information/tractor";
-import tree from "./SDG/SDG13/U1/information/tree";
-import foxHedgehog from "./SDG/SDG13/U1/information/foxHedgehog";
+import akw from "./SDG/SDG13/Spiel/information/akw";
+import cow from "./SDG/SDG13/Spiel/information/cow";
+import bicycle from "./SDG/SDG13/Spiel/information/bicycle";
+import windTurbine from "./SDG/SDG13/Spiel/information/windTurbine";
+import tractor from "./SDG/SDG13/Spiel/information/tractor";
+import tree from "./SDG/SDG13/Spiel/information/tree";
+import foxHedgehog from "./SDG/SDG13/Spiel/information/foxHedgehog";
 
 
-import page141 from "./SDG/SDG14/U1/tutorial/14tut1";
-import page142 from "./SDG/SDG14/U1/tutorial/14tut2";
-import page143 from "./SDG/SDG14/U1/tutorial/14tut3";
+import page141 from "./SDG/SDG14/Spiel2/tutorial/14tut1";
+import page142 from "./SDG/SDG14/Spiel2/tutorial/14tut2";
+import page143 from "./SDG/SDG14/Spiel2/tutorial/14tut3";
 
-import play14 from "./SDG/SDG14/U2/play14";
+import play14 from "./SDG/SDG14/Spiel2/play14";
 
-import clownfish from "./SDG/SDG14/U2/search_info/clownfish";
-import ray from "./SDG/SDG14/U2/search_info/ray";
-import seaHorse from "./SDG/SDG14/U2/search_info/seaHorse";
-import shark from "./SDG/SDG14/U2/search_info/shark";
-import siamesefightingFish from "./SDG/SDG14/U2/search_info/siameseFightingFish";
-import surgeonfish from "./SDG/SDG14/U2/search_info/surgeonfish";
-import turtle from "./SDG/SDG14/U2/search_info/turtle";
-import boat from "./SDG/SDG14/U2/search_info/boat";
+import clownfish from "./SDG/SDG14/Spiel2/search_info/clownfish";
+import ray from "./SDG/SDG14/Spiel2/search_info/ray";
+import seaHorse from "./SDG/SDG14/Spiel2/search_info/seaHorse";
+import shark from "./SDG/SDG14/Spiel2/search_info/shark";
+import siamesefightingFish from "./SDG/SDG14/Spiel2/search_info/siameseFightingFish";
+import surgeonfish from "./SDG/SDG14/Spiel2/search_info/surgeonfish";
+import turtle from "./SDG/SDG14/Spiel2/search_info/turtle";
+import boat from "./SDG/SDG14/Spiel2/search_info/boat";
 
-import { Main } from "./SDG/SDG13/U5/DND/Main";
-import { infotext06 } from "./SDG/SDG6/U0/06infotext";
-import { spiel04 } from "./SDG/SDG4/spiel/04spiel";
-import { infotext11 } from "./SDG/SDG11/U0/11infotext";
-import { infotext13 } from "./SDG/SDG13/U0/13infotext";
-import { infotext14 } from "./SDG/SDG14/U0/14infotext";
-import { tutorial_13_u3 } from "./SDG/SDG13/U3/tutorial_13_U3";
-import { question1_13_u3 } from "./SDG/SDG13/U3/question1_13_U3";
-import { answer1_13_u3 } from "./SDG/SDG13/U3/answer1_13_U3";
-import { answer3_13_u3 } from "./SDG/SDG13/U3/answer3_13_U3";
-import { question3_13_u3 } from "./SDG/SDG13/U3/question3_13_U3";
-import { answer2_13_u3 } from "./SDG/SDG13/U3/answer2_13_U3";
-import { question2_13_u3 } from "./SDG/SDG13/U3/question2_13_U3";
-import { tips_13_u3 } from "./SDG/SDG13/U3/tips_13_U3";
-import { infotext_03_01 } from "./SDG/SDG03/U01/infotext_03_01";
+import { Main } from "./SDG/SDG13/Spiel2/DND/Main";
+import { infotext06 } from "./SDG/SDG6/Info/06infotext";
+import { spiel04 } from "./SDG/SDG4/Spiel/04spiel";
+import { infotext11 } from "./SDG/SDG11/Info/11infotext";
+import { infotext13 } from "./SDG/SDG13/Info/13infotext";
+import { infotext14 } from "./SDG/SDG14/Info/14infotext";
+import { tutorial_13_u3 } from "./SDG/SDG13/Quiz/tutorial_13_U3";
+import { question1_13_u3 } from "./SDG/SDG13/Quiz/question1_13_U3";
+import { answer1_13_u3 } from "./SDG/SDG13/Quiz/answer1_13_U3";
+import { answer3_13_u3 } from "./SDG/SDG13/Quiz/answer3_13_U3";
+import { question3_13_u3 } from "./SDG/SDG13/Quiz/question3_13_U3";
+import { answer2_13_u3 } from "./SDG/SDG13/Quiz/answer2_13_U3";
+import { question2_13_u3 } from "./SDG/SDG13/Quiz/question2_13_U3";
+import { tips_13_u3 } from "./SDG/SDG13/Quiz/tips_13_U3";
+import { infotext_03_01 } from "./SDG/SDG3/Info/infotext_03_01";
 
 // eslint-disable-next-line react/prefer-stateless-function
 export class App extends React.Component {
@@ -90,11 +85,6 @@ export class App extends React.Component {
 
                         <Route exact path="/sdg06/u1/infotext11" component={infotext11} />
 
-                        <Route exact path="/sdg11" component={sdg12} />
-
-                        <Route exact path="/sdg12" component={sdg12} />
-
-                        <Route exact path="/sdg13" component={sdg13} />
                         <Route exact path="/sdg13/u1/infotext13" component={infotext13} />
                         <Route exact path="/sdg13/u3/tutorial1" component={tutorial_13_u3} />
                         <Route exact path="/sdg13/u3/tips" component={tips_13_u3} />
@@ -137,7 +127,8 @@ export class App extends React.Component {
                         <Route exact path="/sdg14/play/siameseFightingFish" component={siamesefightingFish} />
                         <Route exact path="/sdg14/play/surgeonfish" component={surgeonfish} />
                         <Route exact path="/sdg14/play/turtle" component={turtle} />
-                        <Route exact path="/sdg14/play/boat" component={boat} />                       
+                        <Route exact path="/sdg14/play/boat" component={boat} />   
+                        <Route exact path ="/Vorlagen/infotext" component={infotextvorlage}/>                    
                     </Switch>
                 </div>
             </Router>
