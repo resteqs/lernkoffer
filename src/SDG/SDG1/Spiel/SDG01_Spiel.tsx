77521bb91230269b8d744ac25f7a1feb2c43d728
Antoni 'resteqs' Mackowiak <65457699+resteqs@users.noreply.github.com>
2021-10-03 20:32:53 +0200

    v.20211003.1 SDG1(Abbr, Modal)

--- src/SDG/SDG1/Spiel/SDG01_Spiel.tsx
+++ src/SDG/SDG1/Spiel/SDG01_Spiel.tsx
@@ -11,11 +11,14 @@ import infotextbutton from "../../../styles/Infotextbutton.module.css";
 import "../../../styles/colour_sdgs.css";
 import { Link } from "react-router-dom";
 import Logo from "../../../SDGLogos/Goal-SDG01.png";
+import Modal from "react-modal";
 
 
 export const SDG01_Spiel = (): JSX.Element => {
     const sdg = "SDG01";
     let [showtext, setText] = React.useState(1);
+    let [itemSelected, setSelectedItem] = React.useState(0)
+    const [modalIsOpen, setIsOpen] = React.useState(false);
     function incrementCount(): void {
         setText(showtext + 1);
     }
@@ -23,6 +26,17 @@ export const SDG01_Spiel = (): JSX.Element => {
     function decrementCount(): void {
         setText(showtext - 1);
     }
+    function select1(): void
+    {
+        setSelectedItem(1);
+    }
+    function select2(): void
+    {
+        setSelectedItem(2);
+    }
+
+
+
     // eslint-disable-next-line unicorn/consistent-function-scoping
     function toggleExplenation(): void {
         const x = document.getElementById("hover")!;
@@ -32,10 +46,34 @@ export const SDG01_Spiel = (): JSX.Element => {
           x.style.display = "none";
         }
     }
-
+showtext=4
     let buttonNext;
+    let buttonCheck;
     let einleitung;
     let person1_1;
+    let person1_2;
+    let question1;
+    let modal;
+    modal =
+    <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setIsOpen(false)}
+        overlayClassName={{
+          base: "overlay-base",
+          afterOpen: "overlay-after",
+          beforeClose: "overlay-before"
+        }}
+        className={{
+          base: "content-base",
+          afterOpen: "content-after",
+          beforeClose: "content-before"
+        }}
+        closeTimeoutMS={500}
+      >
+        <div className="content-header"><p>Title</p> <button type="button" className="personCheckOkButton" onClick={() => setIsOpen(false)}><b>Okay</b></button></div>
+        
+      </Modal>
+
     buttonNext =
         showtext <= 3 ? (
             <div style={{ marginTop: "10px" }} className="nextbutton">
@@ -45,6 +83,24 @@ export const SDG01_Spiel = (): JSX.Element => {
                     <span className={infotextbutton.edgegreen} />
                     <span className={infotextbutton.front} style={{ backgroundColor: "green" }}>
                         <p className={infotextbutton.buttontext}>Weiter</p> 
+                    </span>
+                </button>
+            </div>
+        ) : (
+            <button className="buttonInvisible" type="button">
+                {" "}
+            </button>
+        );
+
+    buttonCheck =
+        showtext === 4 ? (
+            <div style={{ marginTop: "10px" }} className="personCheckAnswerButton">
+                {" "}
+                <button className={infotextbutton.pushable} type="button" onClick={() => setIsOpen(true)}>
+                    <span className={infotextbutton.shadow} />
+                    <span className={infotextbutton.edgegreen} />
+                    <span className={infotextbutton.front} style={{ backgroundColor: "green" }}>
+                        <p className={infotextbutton.buttontext}>Überprüfe</p> 
                     </span>
                 </button>
             </div>
@@ -78,14 +134,15 @@ export const SDG01_Spiel = (): JSX.Element => {
                 <p style={{ marginTop: "60px", marginLeft: "50px" }} className="underlinedHeader">
                     Person Nr. 1.1: Wirtschaftsinformatiker{" "}
                 </p>
-                <p className="einleitungText">
+                <p className="personText">
                    <ul>
-                    <li>&#9679;Branche: </li>
-                    <li>&#9679;Definition: Arbeit in einem (meist großen) Unternehmen. Beschäftigt sich mit der Technik eines Unternehmens. Genauer gesagt damit, dass wichtige betriebswirtschaftliche Fragen (sagen einer Firma, was sie machen müssen, damit ihre Produkte gut verkauft werden/welche Produkte momentan besonders gut verkauft werden können) <abbr title="mit der Hilfe von Computern und moderner Technik" tabIndex={0}>digital</abbr> gelöst werden </li>  
-                    <li>&#9679; </li>  
-                    <li>&#9679; </li>  
-                    <li>&#9679; </li>  
-                    <li>&#9679; </li>  
+                    <li>&#9679;<b>Branche:</b> IT und Development (= Entwicklung) </li>
+                    <li>&#9679;<b>Definition:</b>Beschäftigt sich mit der Technik eines Unternehmens. Genauer gesagt damit, dass wichtige <abbr title=" (sagen einer Firma, was sie machen müssen, damit ihre Produkte gut verkauft werden/welche Produkte
+                    momentan besonders gut verkauft werden können)" 
+                    tabIndex={0}>betriebswirtschaftliche Fragen</abbr>, <abbr title="mit der Hilfe von Computern und moderner Technik" tabIndex={0}>digital</abbr> gelöst werden. </li>  
+                    <li>&#9679; <b>Voraussetzungen:</b> Abgeschlossenes Studium {"->"} sehr viel in verschiedenen Bereichen (Technik, BWL, Soziologie, Psychologie) lernen und wissen</li>  
+                    <li>&#9679; <b>Mögliche Aufgabengebiete:</b> Mit Kunden kommunizieren und Lösungen für ihre Probleme finden. Diese Lösungsansätze dann Spezialisten erklären und in seltenen Fällen bei der Umsetzung auch selbst helfen.</li>  
+                    <li>&#9679;<b>In welcher Hinsicht anstrengend?:</b> Bei diesem Beruf hast du viele Einflussfaktoren, die deinen Tag anstrengend machen. Zum einen musst du sehr viel Denken und Überlegen. Zum anderen musst du immer an mehrere Sachen gleichzeitig denken, zum Beispiel daran, was der Kunde wirklich will und daran, was deine Kollegen umsetzen können. Du wirst die meiste Zeit vor einem Computer sitzen oder mit Kunden im Gespräch sein, das kann durchaus auch sehr anstrengend sein. </li>  
 
                        </ul>{" "}
                 </p>
@@ -93,6 +150,39 @@ export const SDG01_Spiel = (): JSX.Element => {
         ) : (
             <div />
         );
+        person1_2 =
+        showtext === 3 ? (
+            <div>
+                <p style={{ marginTop: "60px", marginLeft: "50px" }} className="underlinedHeader">
+                    Person Nr. 1.2: Grafiker:In{" "}
+                </p>
+                <p className="personText">
+                   <ul>
+                    <li>&#9679;<b>Branche:</b>Design, Gestaltung und Medien</li>
+                    <li>&#9679;<b>Definition:</b>Erstellung von visuellen Medien (Sachen, die du sehen kannst z.B. Bilder oder Zeichnungen), um die Aufmerksamkeit von Leuten auf zum Beispieleine Werbetafel zu ziehen. Das meiste wird mittlerweile digital erstellt und nur noch wenig auf Papier gezeichnet.</li>  
+                    <li>&#9679; <b>Voraussetzungen:</b> Entweder ein Grafikdesign Studium oder eine Ausbildung zu diesem Beruf. Es können dir aber auch verwandte Studiengänge helfen und dich für die Arbeit als Grafikdesigner:in qualifizieren (zeigen, dass du das kannst). Am wichtigsten ist aber, dass du kreativ bist und zeichnen kannst. In der heutigen Zeit werden auch Medienkompetenzen (Umgang mit Computer und neuen Techniken) immer wichtiger. Außerdem musst du gut mit Kunden umgehen können, weil du schließlich deren Wünsche umsetzen möchtest.</li>  
+                    <li>&#9679; <b>Mögliche Aufgabengebiete:</b> Einige Grafikdesigner erstellen Materialien für Werbungen. Andere arbeiten an Büchern und einige andere auch an Darstellungen für Apps (Anwendungen die auf Computern/Handys/Tablets… laufen).</li>  
+                    <li>&#9679;<b>In welcher Hinsicht anstrengend?:</b> Ein wesentlicher Vorteil ist, dass du deiner Kreativität freien Lauf lassen kannst. Hierbei ist allerdings Vorsicht geboten, denn du musst immer im Kopf behalten, was der Kunde sich wünscht. Bei diesem Beruf ist besonders die Zeit, in der du mit dem Kunden erarbeitest, wie das gewünschte Produkt aussehen soll, anspruchsvoll. Gerade aus dem Grund, weil Kunden oftmals auf deinem Fachgebiet nur wenig Wissen haben und so nicht exakt bestimmen können, was sie brauchen, diesen Schritt musst immer du als Grafiker:in machen. Grafiker:innen arbeiten mittlerweile sehr viel an digitalen Geräten, was auf Dauer auch sehr anstrengend wird, besonders für die Augen und deshalb, weil man die meiste Zeit sitzend verbringt </li>  
+
+                       </ul>{" "}
+                </p>
+            </div>
+        ) : (
+            <div />
+        );
+        question1 =
+        showtext === 4 ? (
+            <div>
+                <p style={{ marginTop: "60px", marginLeft: "50px", fontSize: "30px" }} className="underlinedHeader">
+                Meinst du, dass eine Person, die als Wirtschaftsinformatiker:in oder eine Person, die als Grafikdesigner:in arbeitet mehr verdient? Und warum meinst du, dass das so ist?{" "}
+                </p>
+                 <button type="button" style={{ marginLeft: "150px",marginTop:"60px" }} className={(itemSelected === 1) ? 'selectedPerson': ''} onClick={() => select1()}><img  style={{ maxWidth: "600px", maxHeight: "500px" }}  src="https://picsum.photos/id/237/1000/1000" alt="Wirtschaftsinformatiker"/></button>
+                 <button type="button"style={{ marginRight: "150px",marginTop:"60px", float: "right" }} className={(itemSelected === 2) ? 'selectedPerson': ''} onClick={() => select2()}><img  style={{ maxWidth: "600px", maxHeight: "500px" }}src="https://picsum.photos/id/237/1000/1000" alt="Wirtschaftsinformatiker"/></button>
+            </div>
+        ) : (
+            <div />
+        );
+
 
     return (
         <div>
@@ -101,12 +191,13 @@ export const SDG01_Spiel = (): JSX.Element => {
                     Hochwertige Bildung <img className="sdglogo" alt="logo" src={Logo} />
                 </div>
             </div>
-
-            <div id="hover"><p>test</p></div>
-
             {einleitung}
             {person1_1}
+            {person1_2}
+            {question1}
             {buttonNext}
+            {buttonCheck}
+            {modal}
         </div>
     );
 };
