37f06d9633b24b5461ed8fef2f4eb5cd9b02117e
Antoni 'resteqs' Mackowiak <65457699+resteqs@users.noreply.github.com>
2021-11-16 14:49:32 +0100

    v.20211116.1 qr test

--- src/SDG/Vorlagen/Endscreen/Endscreen.tsx
+++ src/SDG/Vorlagen/Endscreen/Endscreen.tsx
@@ -1,11 +1,53 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import React,  { useState } from 'react'
+import { Fab, TextareaAutosize } from '@material-ui/core'
+import { ArrowBack } from '@material-ui/icons'
+import { Link } from "react-router-dom";
+import QrScan from 'react-qr-reader'
 
-import * as React from "react";
-import endscreen from "../../../styles/endscreen.module.css";
+export const Endscreen = (): JSX.Element => {
 
-export const Endscreen = ():JSX.Element => {
-    return(
-        <div>
-            <p className={endscreen.header}>Ende</p>
-        </div>
-    )
-} 
+    const [qrscan, setQrscan] = useState('No result');
+    const handleScan = (data) => {
+        if (data) {
+            setQrscan(data)
+        }
+    }
+    
+    // eslint-disable-next-line unicorn/consistent-function-scoping
+    const handleError = err => {
+    console.error(err)
+    }
+
+    return (
+      <div>
+            <Link to="/">
+            <Fab style={{ marginRight:10 }} color="primary">
+                <ArrowBack/>
+            </Fab>
+            </Link>
+            <span>QR Scanner</span>
+            
+            
+            <div style={{ marginTop:30 }}>
+                <QrScan
+                    delay={300}
+                    onError={handleError}
+                    onScan={handleScan}
+                    style={{ height: 240, width: 320 }}
+                />
+            </div>
+            
+
+            <TextareaAutosize
+                style={{ fontSize:18, width:320, height:100, marginTop:100 }}
+                rowsMax={4}
+                defaultValue={qrscan}
+                value={qrscan}
+            />
+
+      </div>
+    );
+  }
+  
