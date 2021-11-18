a197df1358f6d53e64e078083025f05c7ef44c84
Antoni 'resteqs' Mackowiak <65457699+resteqs@users.noreply.github.com>
2021-11-18 20:07:54 +0100

    v.20211118.4 SDG11 GB fertig. Endscreen needed!!!!

--- src/SDG/SDG11/Gute Bsp/SDG11_GB.tsx
+++ src/SDG/SDG11/Gute Bsp/SDG11_GB.tsx
@@ -14,10 +14,10 @@ import "../../../styles/infotext.css";
 import Logo from "../../../SDGLogos/Goal-SDG11.png";
 import Bild1 from "./unknown.png";
 import Bild2 from "./unknown1.jpg";
-import Bild3 from "./unknown2.jpg";
+import Bild3 from "./unknown2.png";
 import Bild4 from "./unknown3.jpg";
 import Bild5 from "./unknown4.jpg";
-import Bild6 from "./unknown5.jpg";
+import Bild6 from "./unknows5.jpg";
 import Bild7 from "./unknown6.jpg";
 
 
@@ -43,7 +43,7 @@ export const SDG11_GB = (): JSX.Element => {
                 <li> - Optional: Farbe oder Wolle, um es bunter zu machen; Fäden und Perlen oder Knöpfe (zum Beispiel, wenn du es wie ein Tier gestalten möchtest, das Beine hat</li>
                 </ul>
                 </p>
-                <button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "150px" }} onClick={() => openVorlage() }><img  style={{ maxWidth: "100px", maxHeight: "100px" }} src={Bild1} alt="Wirtschaftsinformatiker"/></button>
+                <button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "150px" }} onClick={() => openBild1() }><img  style={{ maxWidth: "100px", maxHeight: "100px" }} src={Bild1} alt="Wirtschaftsinformatiker"/></button>
             </div>
         ),
         2: (
@@ -52,39 +52,42 @@ export const SDG11_GB = (): JSX.Element => {
                 Nimm die Dose und bemale sie mit der Farbe, oder wickle die Wolle herum.
                 Wenn du es einfach halten möchtest, oder nichts dergleichen zur Hand hast, dann überspringe diesen Schritt einfach.
                 </p>
-                <button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "150px" }} onClick={() => openVorlage() }><img  style={{ maxWidth: "100px", maxHeight: "100px" }} src={Bild2} alt="Wirtschaftsinformatiker"/></button>
+                <button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "150px" }} onClick={() => openBild2() }><img  style={{ maxWidth: "100px", maxHeight: "100px" }} src={Bild2} alt="Wirtschaftsinformatiker"/></button>
             </div>
         ),
         3: (
             <div>
-                <p className="SpielregelnSDG04" style={{ fontSize: "45px" }}>
-                Nimm dir ein Blatt zur Hand und male den Umriss eines Kopfes darauf (wenn du dir nicht sicher bist, wie du den Umriss eines Kopfes zeichnen kannst, dann kannst du einfach hier klicken, dann kommst du zu einer Vorlage, die du einfach abzeichnen kannst) Wenn du das hast, kannst du den Kopf entweder einfach auf dem Blatt lassen oder ausschneiden.
+                <p className="SpielregelnSDG04" style={{ fontSize: "45px" }}> <br/>
+                Am besten lässt du dir hier von einem Erwachsenen helfen:
+                Nimm den Hammer und den Nagel und mache damit ein Loch in den Boden der Dose (Wenn du ein Tier mit Beinen gestalten möchtest, dann mach noch ein paar Löcher in die Dose, durch die du Fäden fädelst und an die Enden Knöpfe oder Perlen knotest).
                 </p>
-                <button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "350px" }} onClick={() => openVorlage() }><img  style={{ maxWidth: "100px", maxHeight: "100px" }} src="https://picsum.photos/id/237/1000/1000" alt="Wirtschaftsinformatiker"/></button>
+                <button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "350px" }} onClick={() => openBild6() }><img  style={{ maxWidth: "100px", maxHeight: "100px" }} src={Bild6} alt="Wirtschaftsinformatiker"/></button>
             </div>
         ),
         4: (
             <div>
-                <p className="SpielregelnSDG04" style={{ fontSize: "45px" }}>
-                Jetzt kannst du all deine Sorgen in den Kopf schreiben und so den anderen zeigen, wie es in deinem Kopf aussieht. Das Wichtigste dabei ist, dass du ehrlich bist und dir bewusst ist, dass du alles schreiben kannst, was du möchtest, denn jeden bedrücken unterschiedliche Sachen.
+                <p className="SpielregelnSDG04" style={{ fontSize: "45px" }}> <br/>
+                Nimm nun den Draht und fädle ihn durch das offene Ende der Dose und das Loch, was du gerade mit dem Nagel gemacht hast.
+                Wenn du damit fertig bist, knote die Enden über der Dose zusammen. Benutze hier ein wenig mehr Draht, weil das dann die Schlaufe wird, an der das Hotel aufgehängt werden soll.
                 </p>
+                <button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "350px" }} onClick={() => openBild3() }><img  style={{ maxWidth: "100px", maxHeight: "100px" }} src={Bild3} alt="Wirtschaftsinformatiker"/></button> 
+                <button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "350px" }} onClick={() => openBild7() }><img  style={{ maxWidth: "100px", maxHeight: "100px" }} src={Bild7} alt="Wirtschaftsinformatiker"/></button> 
             </div>
         ),
         5: (
             <div>
-                <p className="SpielregelnSDG04" style={{ fontSize: "45px" }}>
-                Wenn ihr alle fertig seid, dann setzt euch doch zusammen und wer sich bereit fühlt, der kann den anderen erzählen, was ihn/sie beschäftigt. Ganz wichtig hier ist, dass niemand ausgelacht wird und ihr niemanden zwingt etwas zu sagen.
-                <br/><br/>
-                Vielleicht zeigt euch das, dass es guttun kann, über seine Sorgen zu reden.
+                <p className="SpielregelnSDG04" style={{ fontSize: "45px" }}> <br/>
+                Nimm nun das Füllmaterial und fülle damit die Dose.
+                Achte dabei darauf, dass du genug benutzt, sonst wird es zu locker und alles fällt wieder heraus, wenn der Wind ein bisschen geht.
                 </p>
+                <button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "350px" }} onClick={() => openBild4() }><img  style={{ maxWidth: "100px", maxHeight: "100px" }} src={Bild4} alt="Wirtschaftsinformatiker"/></button> 
+                <button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "350px" }} onClick={() => openBild5() }><img  style={{ maxWidth: "100px", maxHeight: "100px" }} src={Bild5} alt="Wirtschaftsinformatiker"/></button> 
             </div>
         ),
         6: (
             <div>
-                <p className="SpielregelnSDG04" style={{ fontSize: "45px" }}>
-                Wenn ihr alle fertig seid, dann setzt euch doch zusammen und wer sich bereit fühlt, der kann den anderen erzählen, was ihn/sie beschäftigt. Ganz wichtig hier ist, dass niemand ausgelacht wird und ihr niemanden zwingt etwas zu sagen.
-                <br/><br/>
-                Vielleicht zeigt euch das, dass es guttun kann, über seine Sorgen zu reden.
+                <p className="SpielregelnSDG04" style={{ fontSize: "45px" }}> <br/>
+                Nachdem dein Insektenhotel jetzt fertig ist, nimm es und suche dir einen guten Platz in der Natur (Tipp: ein guter Platz ist immer in einem Baum).
                 </p>
             </div>
         ),
@@ -92,32 +95,32 @@ export const SDG11_GB = (): JSX.Element => {
         61: (
          <div><div><img  style={{ maxWidth: "600px", maxHeight: "700px", marginTop: "-70px" }}src={Bild1} alt="vorlage"/>
          </div>
-        <div><button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "150px", fontSize: "30px" }} onClick={() => closeVorlage() }>Zurück</button></div></div> 
+        <div><button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "150px", fontSize: "30px" }} onClick={() => closeBild1() }>Zurück</button></div></div> 
         ),
-        62: ( <div><div><img  style={{ maxWidth: "600px", maxHeight: "700px", marginTop: "-70px" }}src="https://picsum.photos/id/237/1000/1000" alt="vorlage"/>
+        62: ( <div><div><img  style={{ maxWidth: "600px", maxHeight: "700px", marginTop: "-70px" }}src={Bild2} alt="vorlage"/>
             </div>
-           <div><button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "150px", fontSize: "30px" }} onClick={() => closeVorlage() }>Zurück</button></div></div> 
+           <div><button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "150px", fontSize: "30px" }} onClick={() => closeBild2() }>Zurück</button></div></div> 
            ),
-        63: ( <div><div><img  style={{ maxWidth: "600px", maxHeight: "700px", marginTop: "-70px" }}src="https://picsum.photos/id/237/1000/1000" alt="vorlage"/>
+        63: ( <div><div><img  style={{ maxWidth: "600px", maxHeight: "700px", marginTop: "-70px" }}src={Bild3} alt="vorlage"/>
            </div>
-          <div><button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "150px", fontSize: "30px" }} onClick={() => closeVorlage() }>Zurück</button></div></div> 
+          <div><button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "150px", fontSize: "30px" }} onClick={() => closeBild3() }>Zurück</button></div></div> 
           ),
         
-        64: ( <div><div><img  style={{ maxWidth: "600px", maxHeight: "700px", marginTop: "-70px" }}src="https://picsum.photos/id/237/1000/1000" alt="vorlage"/>
+        64: ( <div><div><img  style={{ maxWidth: "600px", maxHeight: "700px", marginTop: "-70px" }}src={Bild4} alt="vorlage"/>
           </div>
-         <div><button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "150px", fontSize: "30px" }} onClick={() => closeVorlage() }>Zurück</button></div></div> 
+         <div><button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "150px", fontSize: "30px" }} onClick={() => closeBild4() }>Zurück</button></div></div> 
          ),
-         65: ( <div><div><img  style={{ maxWidth: "600px", maxHeight: "700px", marginTop: "-70px" }}src="https://picsum.photos/id/237/1000/1000" alt="vorlage"/>
+         65: ( <div><div><img  style={{ maxWidth: "600px", maxHeight: "700px", marginTop: "-70px" }}src={Bild5} alt="vorlage"/>
          </div>
-        <div><button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "150px", fontSize: "30px" }} onClick={() => closeVorlage() }>Zurück</button></div></div> 
+        <div><button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "150px", fontSize: "30px" }} onClick={() => closeBild5() }>Zurück</button></div></div> 
         ),
-        66: ( <div><div><img  style={{ maxWidth: "600px", maxHeight: "700px", marginTop: "-70px" }}src="https://picsum.photos/id/237/1000/1000" alt="vorlage"/>
+        66: ( <div><div><img  style={{ maxWidth: "600px", maxHeight: "700px", marginTop: "-70px" }}src={Bild6} alt="vorlage"/>
         </div>
-       <div><button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "150px", fontSize: "30px" }} onClick={() => closeVorlage() }>Zurück</button></div></div> 
+       <div><button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "150px", fontSize: "30px" }} onClick={() => closeBild6() }>Zurück</button></div></div> 
        ),
-       67: ( <div><div><img  style={{ maxWidth: "600px", maxHeight: "700px", marginTop: "-70px" }}src="https://picsum.photos/id/237/1000/1000" alt="vorlage"/>
+       67: ( <div><div><img  style={{ maxWidth: "600px", maxHeight: "700px", marginTop: "-70px" }}src={Bild7} alt="vorlage"/>
        </div>
-      <div><button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "150px", fontSize: "30px" }} onClick={() => closeVorlage() }>Zurück</button></div></div> 
+      <div><button type="button" style={{ background: "none", borderColor :"blue" , borderWidth: "11px", borderRadius: "15px", marginTop: "150px", fontSize: "30px" }} onClick={() => closeBild7() }>Zurück</button></div></div> 
       ),
 
 
@@ -162,11 +165,47 @@ export const SDG11_GB = (): JSX.Element => {
     function decrementCount(): void {
         setText(showtext - 1);
     }
-    function openVorlage(): void {
-        setText(69);
+    function openBild1(): void {
+        setText(61);
     }
-    function closeVorlage(): void{
+    function openBild2(): void {
+        setText(62);
+    }
+    function openBild3(): void {
+        setText(63);
+    }
+    function openBild4(): void {
+        setText(64);
+    }
+    function openBild5(): void {
+        setText(65);
+    }
+    function openBild6(): void {
+        setText(66);
+    }
+    function openBild7(): void {
+        setText(67);
+    }
+    function closeBild1(): void{
+        setText(1);
+    }
+    function closeBild2(): void{
+        setText(2);
+    }
+    function closeBild3(): void{
+        setText(4);
+    }
+    function closeBild4(): void{
+        setText(5);
+    }
+    function closeBild5(): void{
+        setText(5);
+    }
+    function closeBild6(): void{
         setText(3);
+    }
+    function closeBild7(): void{
+        setText(4);
     }
     let buttonBack;
     let buttonNext;
