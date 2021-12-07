f38c1a502ad784c33a2fb8823e688b18141f3477
misterrabenklaue <79755431+misterrabenklaue@users.noreply.github.com>
2021-12-07 18:02:57 +0100

    v.20211207 v1 Implementation of SDG 02 Infotext

--- src/SDG/SDG2/Info/Infotext/SDG02_Infotext.tsx
+++ src/SDG/SDG2/Info/Infotext/SDG02_Infotext.tsx
@@ -1,37 +1,54 @@
 // eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable eslint-comments/no-duplicate-disable */
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable react/jsx-pascal-case */
+/* eslint-disable eslint-comments/disable-enable-pair */
+// eslint-disable-next-line eslint-comments/no-duplicate-disable
+// eslint-disable-next-line eslint-comments/disable-enable-pair
 /* eslint-disable no-eval */
+/* eslint-disable unicorn/filename-case */
 
-// eslint-disable-next-line unicorn/filename-case
+
 import * as React from "react";
 import renderer from "react-dom";
-import infotextbutton from "../../../../styles/Infotextbutton.module.css";
+import { Link } from 'react-router-dom';
+import infotextbutton from "../../../../styles/Infotextbutton.module.css" ;
 import "../../../../styles/colour_sdgs.css";
-import { Link } from "react-router-dom";
 import "../../../../styles/infotext.css";
 import Logo from "../../../../SDGLogos/Goal-SDG02.png";
+import EG from "../../../../EG/EG2.png"
+import "../../Spiel/sdg02.css";
+import ReactAudioPlayer from 'react-audio-player';
+
+
+
+
 
 export const SDG02_Infotext = (): JSX.Element => {
     // eslint-disable-next-line prefer-const
     let [showtext, setText] = React.useState(1);
     const sdg = "SDG02";
+   
 
     const text = {
-        1: `Extremer Hunger und Unterernährung sind nach wie vor ein Hindernis für eine nachhaltige Entwicklung. Sie schaffen eine Falle, aus der sich die Menschen nicht so leicht befreien können, denn Hunger und Unterernährung bedeuten weniger produktive Menschen. Sie können schneller krank werden und sind dann oft nicht in der Lage, mehr zu verdienen und ihre Lebenshaltungskosten zu verbessern.`,
-        2: `Menschen, die von "moderater Ernährungsunsicherheit" betroffen sind, sind in der Regel aufgrund von Einkommens- oder anderen Ressourcenbeschränkungen nicht in der Lage, sich regelmäßig gesund und ausgewogen zu ernähren. Konflikte, Klimaschocks, Heuschrecken-Krisen und Pandemien stellen eine Bedrohung für die Ernährungssysteme dar. Unsicherheit der Bürger:innen und rückläufige Nahrungsmittelproduktion haben zu.`,
-        3: "Nahrungsmittelknappheit und hohen Nahrungsmittelpreisen beigetragen. Investitionen in dem Agrarsektor (= Landwirtschaft) sind entscheidend für die Reduzierung von Hunger und Armut, die Verbesserung der Ernährungssicherheit, die Schaffung von Arbeitsplätzen und den Aufbau von Widerstandsfähigkeit gegenüber Katastrophen und Schocks.",
-        4: "Eine Welt ohne Hunger kann sich positiv auf unsere Volkswirtschaften, Gesundheit, Bildung, Gleichberechtigung und soziale Entwicklung auswirken. Sie ist sehr wichtig, um eine bessere Zukunft für alle zu schaffen. Darüber hinaus werden wir mit Hunger, der die menschliche Entwicklung einschränkt, nicht in der Lage sein, die anderen Nachhaltigkeitsziele wie Bildung, Gesundheit und Gleichberechtigung der Geschlechter zu erreichen.",
-        5: "Um Ernährungssicherheit zu erreichen, müssen wir uns mit mehreren Themen befassen, wie z. B. dem sozialen Schutz, um sichere und nahrhafte Lebensmittel insbesondere für Kinder zu gewährleisten. Ziel ist es, die Ernährungssysteme zu verändern, um eine inklusivere und nachhaltigere Welt zu erreichen.",
-        6: 'Du kannst helfen, indem du lokale Landwirte oder Märkte unterstützen und dich für nachhaltige Lebensmittel entscheidest, eine gute Ernährung für alle unterstützt und Lebensmittelverschwendung bekämpfst. ',
-        7: 'Als Verbraucher:in und zukünftige:r Wähler:in kannst du Unternehmen und deine Regierung auffordern, Entscheidungen und Veränderungen zu treffen, die dieses Nachhaltigkeitsziel unterstützen. Wenn in deinen lokalen Gemeinden Diskussionen stattfinden, kannst du dich ihnen anschließen und dich für das Ziel "Kein Hunger" einsetzen.',
+        1: <div><p><b>„den Hunger beenden, Ernährungssicherheit und eine bessere Ernährung erreichen und eine nachhaltige Landwirtschaft fördern“</b></p></div>,
+        2: <div><p className="Infotext02">Wenn Menschen extrem hungern und unterernährt sind, kann es keine nachhaltige Entwicklung geben. Da sie so wenig Essen haben, werden sie geschwächt und können schneller krank werden. Infolgedessen können sie nicht mehr so viel arbeiten und verdienen weniger Geld. Ohne das können sie nicht mehr Essen kaufen. Das heißt, dass durch den Hunger und die Unterernährung eine Falle entsteht, aus welcher sie sich nicht so einfach befreien können. </p></div>,
+        3: <div><p className="Infotext02">Es gibt Menschen, die sich nicht regelmäßig gesund und ausgewogen, also abwechslungsreich, ernähren können. Das liegt dann daran, dass manche Dinge, wie Geld oder Nahrung, nicht ausreichend vorhanden sind.</p></div>,
+        4: <div><p className="Infotext02">Ein Ernährungssystem (Anbauen &#10140; Verarbeiten &#10140; Verpacken &#10140; Transportieren &#10140; Verteilen &#10140; Handeln &#10140; Einkaufen &#10140; Zubereiten &#10140; Verzehren von Essen) kann von Konflikten, Klimakrisen, Heuschrecken-Plagen und Pandemien bedroht werden.</p></div>,
+        5: <div><p className="Infotext02">Wenn die Bürger:innen unsicher werden, produzieren sie weniger Nahrung, was dazu führt, dass es weniger Essen gibt und die Preise dafür steigen. Deshalb ist es wichtig, dass der Agrarsektor (= Landwirtschaft) mit Geld unterstützt wird. Dadurch können Hunger und Armut verringert werden, die Ernährung wird besser gesichert, es entstehen mehr Arbeitsplätze und die Anfälligkeit für Katastrophen oder andere Bedrohungen sinkt.</p></div>,
+        6: <div><p className="Infotext02">Eine Welt ohne Hunger kann unser Arbeitsleben, unsere Gesundheit, Bildung, Gleichberechtigung und gesellschaftliche Entwicklung verbessern. Sie ist sehr wichtig, um eine bessere Zukunft für alle zu schaffen. Darüber hinaus sind wir mit Hunger nicht in der Lage die anderen Nachhaltigkeitsziele zu erreichen, da er dazu führt, dass wir uns nicht mehr so gut weiterentwickeln können. </p></div>,
+        7: <div><p className="Infotext02">Um Ernährung sicher zu stellen, müssen wir uns mit mehreren Themen befassen: z. B. mit dem Schutz der Gesellschaft, um sichere und nahrhafte Lebensmittel besonders für Kinder zu gewährleisten. Ein anderes Beispiel ist die Veränderung der Ernährungssysteme, damit die Welt nachhaltiger und inklusiver (= mehr alle miteinbeziehen) wird. Du kannst helfen, indem du bei Landwirten oder Märkten aus deiner Gegend einkaufst und dich für nachhaltige Lebensmittel entscheidest.</p></div>,
+        8: <div><p className="Infotext02">Außerdem ist es wichtig, eine gute Ernährung für alle zu unterstützen und Lebensmittelverschwendung zu bekämpfen. Auch Unternehmen und deine Regierung sollten Entscheidungen und Veränderungen treffen, die dieses Nachhaltigkeitsziel unterstützen. Bei diesen, sowie in deiner Gemeinde zählt deine Stimme als zukünftige:r Wähler:in und Verbraucher:in von Nahrung.</p></div>
     };
     const header = {
-        1: "Warum?",
-        2: "Was ist das Problem?",
-        3: "Was ist das Problem?",
-        4: "Wie ist dieses Thema mit uns verbunden?",
-        5: "Was können wir machen?",
-        6: "Was können wir machen?",
-        7: "Was können wir machen?",
+        1: <div><p> </p></div>,
+        2: <div><p>Warum?</p></div>,
+        3: <div><p>Was ist das Problem?</p></div>,
+        4: <div><p>Was ist das Problem?</p></div>,
+        5: <div><p>Was ist das Problem?</p></div>,
+        6: <div><p>Wie ist dieses Thema mit uns verbunden?</p></div>,
+        7: <div><p>Was können wir tun?</p></div>,
+        8: <div><p>Was können wir tun?</p></div>,
     };
 
     function incrementCount(): void {
@@ -44,20 +61,17 @@ export const SDG02_Infotext = (): JSX.Element => {
     let buttonBack;
     let buttonNext;
     let buttonEnd;
-
     // eslint-disable-next-line prefer-const
     buttonBack =
         showtext >= 2 ? (
-            <div className="backbutton">
-                {" "}
-                <button className={infotextbutton.pushable} type="button" onClick={decrementCount}>
-                    <span className={infotextbutton.shadow} />
-                    <span className={infotextbutton.edge} />
-                    <span className={infotextbutton.front}>
-                        <p className={infotextbutton.buttontext}>Zurück</p>
-                    </span>
-                </button>
-            </div>
+            <div className="backbutton"> <button className= {infotextbutton.pushable} type = "button" onClick={decrementCount} >
+            <span className={infotextbutton.shadow} />
+            <span className={infotextbutton.edge} />
+            <span className={infotextbutton.front}>
+               <p className={infotextbutton.buttontext}>Zurück</p> 
+            </span>
+            </button></div>
+           
         ) : (
             <button className="buttonInvisible" type="button">
                 {" "}
@@ -65,51 +79,50 @@ export const SDG02_Infotext = (): JSX.Element => {
         );
     // eslint-disable-next-line prefer-const
     buttonNext =
-        showtext <= 5 ? (
-            <div className="nextbutton">
-                {" "}
-                <button className={infotextbutton.pushable} type="button" onClick={incrementCount}>
-                    <span className={infotextbutton.shadow} />
-                    <span className={infotextbutton.edgegreen} />
-                    <span className={infotextbutton.front} style={{ backgroundColor: "green" }}>
-                        <p className={infotextbutton.buttontext}>Weiter</p>
-                    </span>
-                </button>
-            </div>
+        showtext < 8 ? (
+            <div className="nextbutton"> <button className= {infotextbutton.pushable} type = "button" onClick={incrementCount}>
+            <span className={infotextbutton.shadow} />
+            <span className={infotextbutton.edgegreen} />
+            <span className={infotextbutton.front} style = { { backgroundColor: "green" } } >
+                <p className={infotextbutton.buttontext}>Weiter</p> 
+            </span>
+            </button></div>
         ) : (
             <button className="buttonInvisible" type="button">
                 {" "}
             </button>
         );
-    // eslint-disable-next-line prefer-const
-    buttonEnd =
-        showtext === 6 ? (
+        // eslint-disable-next-line prefer-const
+        buttonEnd =
+        showtext === 8? (
             <div className="nextbutton">
+               <div style={{ marginTop: "10px" }} className="nextbutton">
+                {" "}
                 <Link to="/Vorlagen/Endscreen">
                     {" "}
-                    <button className={infotextbutton.pushable} type="button">
-                        <span className={infotextbutton.shadow} />
-                        <span className={infotextbutton.edgegreen} />
-                        <span className={infotextbutton.front} style={{ backgroundColor: "green" }}>
-                            <p className={infotextbutton.buttontext}>Weiter </p>
-                        </span>
-                    </button>
+                    <div>
+                        <button className={infotextbutton.pushable} type="button" onClick={incrementCount}>
+                            <span className={infotextbutton.shadow} />
+                            <span className={infotextbutton.edgegreen} />
+                            <span className={infotextbutton.front} style={{ backgroundColor: "green" }}>
+                                <p className={infotextbutton.buttontext}>Weiter</p>
+                            </span>
+                        </button>
+                    </div>
                 </Link>{" "}
+            </div>
             </div>
         ) : (
             <button className="buttonInvisible" type="button">
                 {" "}
             </button>
         );
-    
 
-    // eslint-disable-next-line prefer-const
     return (
         <div>
             <div className={`colour_${sdg}_header`}>
                 <div className="header">
-                    Kein Hunger
-                    <img className="sdglogo" alt="logo" src={Logo} />
+                    Kein Hunger <img className="sdglogo" alt="logo" src={Logo} />
                 </div>
             </div>
             <div className="infotextbox">
@@ -122,8 +135,12 @@ export const SDG02_Infotext = (): JSX.Element => {
             {buttonBack}
             {buttonNext}
             {buttonEnd}
+            <ReactAudioPlayer
+  src="https://cdn.discordapp.com/attachments/807650919780057129/916030116767891456/Mobiles_Museum_Infotext_4.mp3"
+  controls
+  style={{ position: 'absolute' ,top:"1100px", left:"80px" }}
+/>  <img className="EG "alt="" src={EG}/>
         </div>
     );
 };
-// eslint-disable-next-line react/jsx-pascal-case
 renderer.render(<SDG02_Infotext />, document.getElementById("root"));
