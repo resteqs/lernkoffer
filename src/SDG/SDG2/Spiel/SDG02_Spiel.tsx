bf5897a40a6cf39c0f5bcaf9f5cc5d8390d7e8ba
Antoni 'resteqs' Mackowiak <65457699+resteqs@users.noreply.github.com>
2021-12-09 22:31:36 +0100

    v.20211209 SDG 02 logic

--- src/SDG/SDG2/Spiel/SDG02_Spiel.tsx
+++ src/SDG/SDG2/Spiel/SDG02_Spiel.tsx
@@ -1,4 +1,6 @@
 // eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable prefer-const */
+// eslint-disable-next-line eslint-comments/disable-enable-pair
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 // eslint-disable-next-line eslint-comments/disable-enable-pair
 /* eslint-disable no-eval */
@@ -6,8 +8,11 @@
 // eslint-disable-next-line unicorn/filename-case
 import * as React from "react";
 import renderer from "react-dom";
-import infotextbutton from "../../../styles/Infotextbutton.module.css";
 import { Link } from "react-router-dom";
+import { CSSProperties, useState } from "react";
+import { faAlignCenter } from "@fortawesome/free-solid-svg-icons";
+import { debug } from "console";
+import infotextbutton from "../../../styles/Infotextbutton.module.css";
 import Logo from "../../../SDGLogos/Goal-SDG02.png";
 import "./sdg02.css";
 
@@ -18,34 +23,41 @@ import pumpkin from "./media/pumpkin.jpg";
 import sushi from "./media/sushi.jpg";
 import tajine from "./media/tajine.jpg";
 
-import { CSSProperties } from "react";
-import { faAlignCenter } from "@fortawesome/free-solid-svg-icons";
-import { debug } from "console";
-
 export const SDG02_Spiel = (): JSX.Element => {
-    // eslint-disable-next-line prefer-const
-    let [showtext, setText] = React.useState(1);
-    let obj1:boolean; let obj2:boolean; let obj3:boolean;
+    let [showtext, setText] = React.useState(5);
+
+    let [currentlyPicked, setPicked] = React.useState(1);
+
+    let [obj1, setObj1] = React.useState(false);
+    let [obj2, setObj2] = React.useState(false);
+    let [obj3, setObj3] = React.useState(false);
+    let [obj4, setObj4] = React.useState(false);
+    let [obj5, setObj5] = React.useState(false);
+
+    let [obj1number, setObj1number] = React.useState(0);
+    let [obj2number, setObj2number] = React.useState(0);
+    let [obj3number, setObj3number] = React.useState(0);
+    let [obj4number, setObj4number] = React.useState(0);
+    let [obj5number, setObj5number] = React.useState(0);
     const sdg = "SDG02";
 
-    const noborder:CSSProperties = {
+    const noborder: CSSProperties = {
         background: "orange",
         fontSize: "30px",
-    }
-    
+        padding: "50px 40px",
+    };
+
     const withborder: CSSProperties = {
         background: "orange",
         fontSize: "30px",
-        border: "black 10px solid",
-    }
+        padding: "0px 40px",
+    };
 
-    
     let buttonStyle = noborder;
 
-    function toggleClass():any {
-        (buttonStyle === noborder) ? (buttonStyle = noborder):(buttonStyle === noborder);
-
-        
+    function toggleClass(): any {
+        buttonStyle === noborder ? (buttonStyle = withborder) : buttonStyle === noborder;
+        incrementCount();
     }
 
     function incrementCount(): void {
@@ -57,15 +69,144 @@ export const SDG02_Spiel = (): JSX.Element => {
     }
 
     // eslint-disable-next-line unicorn/consistent-function-scoping
-    function obj1select(): void {
-        obj1 = !obj1;        
-    }
-    function obj2select(): void {
-        obj1 = !obj1;        
-    }
-    function obj3select(): void {
-        obj1 = !obj1;        
-    }
+
+    let button1 =
+        obj1 === false ? (
+            <div style={{ marginBottom: "50px", paddingBottom: "20px" }}>
+                <button
+                    className="pushable"
+                    type="button"
+                    onClick={() => {
+                        setObj1number(currentlyPicked);
+                        setPicked(currentlyPicked + 1);
+                        incrementCount();
+                        setObj1(true);
+                    }}
+                >
+                    <div style={{ width: "768px" }}>
+                        <span className="shadow" />
+                        <span className={infotextbutton.edgeorange} />
+                        <span className="front" style={buttonStyle}>
+                            Nord Amerika
+                        </span>
+                    </div>
+                </button>{" "}
+            </div>
+        ) : (
+            <div>{}</div>
+        );
+    let button2 =
+        obj2 === false ? (
+            <div style={{ marginBottom: "50px", paddingBottom: "20px" }}>
+                <button
+                    className="pushable"
+                    type="button"
+                    onClick={() => {
+                        setObj2number(currentlyPicked);
+                        setPicked(currentlyPicked + 1);
+                        incrementCount();
+                        setObj2(true);
+                    }}
+                >
+                    <div style={{ width: "768px" }}>
+                        <span className="shadow" />
+                        <span className={infotextbutton.edgeorange} />
+                        <span className="front" style={buttonStyle}>
+                            Nord Amerika
+                        </span>
+                    </div>
+                </button>{" "}
+            </div>
+        ) : (
+            <div>{}</div>
+        );
+    let button3 =
+        obj3 === false ? (
+            <div style={{ marginBottom: "50px", paddingBottom: "20px" }}>
+                <button
+                    className="pushable"
+                    type="button"
+                    onClick={() => {
+                        setObj3number(currentlyPicked);
+                        setPicked(currentlyPicked + 1);
+                        incrementCount();
+                        setObj3(true);
+                    }}
+                >
+                    <div style={{ width: "768px" }}>
+                        <span className="shadow" />
+                        <span className={infotextbutton.edgeorange} />
+                        <span className="front" style={buttonStyle}>
+                            Nord Amerika
+                        </span>
+                    </div>
+                </button>{" "}
+            </div>
+        ) : (
+            <div>{}</div>
+        );
+    let button4 =
+        obj4 === false ? (
+            <div style={{ marginBottom: "50px", paddingBottom: "20px" }}>
+                <button
+                    className="pushable"
+                    type="button"
+                    onClick={() => {
+                        setObj4number(currentlyPicked);
+                        setPicked(currentlyPicked + 1);
+                        incrementCount();
+                        setObj4(true);
+                    }}
+                >
+                    <div style={{ width: "768px" }}>
+                        <span className="shadow" />
+                        <span className={infotextbutton.edgeorange} />
+                        <span className="front" style={buttonStyle}>
+                            Nord Amerika
+                        </span>
+                    </div>
+                </button>{" "}
+            </div>
+        ) : (
+            <div>{}</div>
+        );
+    let button5 =
+        obj5 === false ? (
+            <div style={{ marginBottom: "50px", paddingBottom: "20px" }}>
+                <button
+                    className="pushable"
+                    type="button"
+                    onClick={() => {
+                        setObj5number(currentlyPicked);
+                        setPicked(currentlyPicked + 1);
+                        incrementCount();
+                        setObj5(true);
+                    }}
+                >
+                    <div style={{ width: "768px" }}>
+                        <span className="shadow" />
+                        <span className={infotextbutton.edgeorange} />
+                        <span className="front" style={buttonStyle}>
+                            Nord Amerika
+                        </span>
+                    </div>
+                </button>{" "}
+            </div>
+        ) : (
+            <div>{}</div>
+        );
+
+    let buttons = (
+        <section style={{ display: "block", float: "right" }}>
+            <div>
+                {button1}
+                {button2}
+                {button3}
+                {button4}
+                {button5}
+            </div>
+        </section>
+    );
 
     const text = {
         1: (
@@ -118,53 +259,35 @@ export const SDG02_Spiel = (): JSX.Element => {
                 <img className="center" src={pumpkin} alt="" />
             </div>
         ),
-        
+
         5: (
             <div>
                 <section>
                     <div style={{ float: "left", maxWidth: "50%", marginBottom: "120px" }}>
                         <p>
-                            Beschreibung: Das ist eine Kürbissuppe. Bei der Zubereitung nimmt man einen Kürbis, schneidet
-                            ihn in kleine Stücken und kocht diese, bis sie weich sind. Wenn die Stückchen fertiggekocht
-                            sind, dann püriert man sie und gibt noch etwas Flüssigkeit (z.B. Wasser oder Brühe) dazu, damit
-                            die Suppe flüssiger wird.{" "}
+                            Beschreibung: Das ist eine Kürbissuppe. Bei der Zubereitung nimmt man einen Kürbis,
+                            schneidet ihn in kleine Stücken und kocht diese, bis sie weich sind. Wenn die Stückchen
+                            fertiggekocht sind, dann püriert man sie und gibt noch etwas Flüssigkeit (z.B. Wasser oder
+                            Brühe) dazu, damit die Suppe flüssiger wird.{" "}
                         </p>
                     </div>
                 </section>
-                
-                <section style = {{ display: "block", float: "right" }}> 
-                    <div >    
-                        <div style = {{ marginBottom : "50px" }}>
-                            <button className = "pushable" type="button" onClick = { () => {(buttonStyle === noborder) ? (buttonStyle = withborder):( " " ) }} >
-                                <div style = {{ width: "768px" }}>
-                                    <span className="shadow" style = {{ backgroundColor: "cc7000" }} />
-                                    <span className={infotextbutton.edgeorange} />
-                                    <span className="front" style = { buttonStyle }>Europa</span>
-                                </div>
-                            </button> { " " }
-                        </div>
-                        <div style = {{ marginBottom : "50px", paddingBottom: "20px" }}>
-                            <button className = "pushable" type="button" onClick = { () => toggleClass()}> 
-                                <div style = {{ width: "768px" }}>
-                                    <span className="shadow" />
-                                    <span className={infotextbutton.edgeorange} />
-                                    <span className="front" style = { buttonStyle }>Amerika</span>
-                                </div>
-                            </button> { " " }
-                        </div>
-                        <div style = {{ marginBottom : "50px", paddingBottom: "20px" }}>
-                            <button className = "pushable" type="button" onClick = { () => toggleClass()}>
-                                <div style = {{ width: "768px" }}>
-                                    <span className="shadow" />
-                                    <span className={infotextbutton.edgeorange} />
-                                    <span className= "front " style = { buttonStyle }> 
-                                        Afrika
-                                    </span>
-                                </div>
-                            </button> { " " }
-                        </div>
+                {buttons}
+            </div>
+        ),
+        6: (
+            <div>
+                <section>
+                    <div style={{ float: "left", maxWidth: "50%", marginBottom: "120px" }}>
+                        <p>
+                            Beschreibung: Das ist eine Kürbissuppe. Bei der Zubereitung nimmt man einen Kürbis,
+                            schneidet ihn in kleine Stücken und kocht diese, bis sie weich sind. Wenn die Stückchen
+                            fertiggekocht sind, dann püriert man sie und gibt noch etwas Flüssigkeit (z.B. Wasser oder
+                            Brühe) dazu, damit die Suppe flüssiger wird.{" "}
+                        </p>
                     </div>
                 </section>
+                {buttons}
             </div>
         ),
 
@@ -286,27 +409,24 @@ export const SDG02_Spiel = (): JSX.Element => {
         ),
     };
 
-    
     let buttonBack;
     let buttonNext;
     let buttonEnd;
     let showobject1;
     let showobject2;
 
-    // eslint-disable-next-line prefer-const
     buttonBack =
         showtext >= 2 ? (
             <div style={{ marginTop: "1px" }} className="backButtonDiv">
-            {" "}
-            <button type="button" onClick={decrementCount} className="backButton">
-                &#129044;
-            </button>
-        </div>
-    ) : (
-        <div> </div>
-    );
+                {" "}
+                <button type="button" onClick={decrementCount} className="backButton">
+                    &#129044;
+                </button>
+            </div>
+        ) : (
+            <div> </div>
+        );
 
-    // eslint-disable-next-line prefer-const
     buttonNext =
         showtext <= 13 ? (
             <div style={{ marginTop: "10px" }} className="nextbutton">
@@ -326,8 +446,7 @@ export const SDG02_Spiel = (): JSX.Element => {
                 {" "}
             </button>
         );
-    
-    // eslint-disable-next-line prefer-const
+
     buttonEnd =
         showtext === 14 ? (
             <div style={{ marginTop: "10px" }} className="nextbutton">
