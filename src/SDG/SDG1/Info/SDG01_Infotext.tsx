4da8aac260ba1a2d531ebf834d44b6c543ed364a
Skydreamer-123 <danielungar1@gmx.de>
2021-12-11 15:44:00 +0100

    v. 20211211 v.2 finished the implementation of SDG 01 Infotext

--- src/SDG/SDG1/Info/SDG01_Infotext.tsx
+++ src/SDG/SDG1/Info/SDG01_Infotext.tsx
@@ -13,99 +13,80 @@ import EG from "./eg/EG.png";
 // eslint-disable-next-line import/no-duplicates
 import audiofile from "./eg/EG.png";
 
-
 export interface content {
-    id:number;
-    text:JSX.Element
+    id: number;
+    text: JSX.Element;
 }
 
-
 export const SDG01_Infotext = (): JSX.Element => {
-
     const [showtext, setText] = React.useState(1);
     const sdg = "SDG01";
     const sdgNumber = 1;
-    const maxSlides = 8;
+    const maxSlides = 7;
 
-    const text= {
+    const text = {
         1: (
             <div>
                 <p>
-                    <b>
-                        „den Hunger beenden, Ernährungssicherheit und eine bessere Ernährung erreichen und eine
-                        nachhaltige Landwirtschaft fördern“
-                    </b>
+                    <b>“Verfügbarkeit von Jobs und einem gerechten Gehalt für alle gewährleisten”</b>
                 </p>
             </div>
         ),
         2: (
             <div>
                 <p className="Infotext02">
-                    Wenn Menschen extrem hungern und unterernährt sind, kann es keine nachhaltige Entwicklung geben. Da
-                    sie so wenig Essen haben, werden sie geschwächt und können schneller krank werden. Infolgedessen
-                    können sie nicht mehr so viel arbeiten und verdienen weniger Geld. Ohne das können sie nicht mehr
-                    Essen kaufen. Das heißt, dass durch den Hunger und die Unterernährung eine Falle entsteht, aus
-                    welcher sie sich nicht so einfach befreien können.{" "}
+                    Ein sicherer Arbeitsplatz und vor allem faires Gehalt für jeden sind für die Struktur einer
+                    Gesellschaft enorm wichtig. Viele Aspekte wie Ernährungssicherheit, Frieden und Bildung hängen stark
+                    davon ab. Ein fester Job bildet aber auch die Grundlage für ein Leben, in dem ihr alle
+                    Grundbedürfnisse befriedigen könnt, wie sich zu ernähren und ein Dach über dem Kopf zu haben.
+                    Außerdem ist es wahrscheinlicher, dass Kinder, die in Armut leben, schlechte oder sogar gar keine
+                    Bildung erfahren.{" "}
                 </p>
             </div>
         ),
         3: (
             <div>
                 <p className="Infotext02">
-                    Es gibt Menschen, die sich nicht regelmäßig gesund und ausgewogen, also abwechslungsreich, ernähren
-                    können. Das liegt dann daran, dass manche Dinge, wie Geld oder Nahrung, nicht ausreichend vorhanden
-                    sind.
+                    Im Jahre 2015 waren 700 Millionen Menschen von extremer Armut betroffen (zum Vergleich: 2020 lebten
+                    fast 8 Milliarden Menschen auf der Welt). Extreme Armut betrifft Menschen die mit weniger als 1,57€
+                    am Tag leben müssen und so ihre Grundbedürfnisse nicht mehr befriedigen können.
                 </p>
             </div>
         ),
         4: (
             <div>
                 <p className="Infotext02">
-                    Ein Ernährungssystem (Anbauen &#10140; Verarbeiten &#10140; Verpacken &#10140; Transportieren
-                    &#10140; Verteilen &#10140; Handeln &#10140; Einkaufen &#10140; Zubereiten &#10140; Verzehren von
-                    Essen) kann von Konflikten, Klimakrisen, Heuschrecken-Plagen und Pandemien bedroht werden.
+                    Aber Armut ist nicht nur auf schlechtes Einkommen zurückzuführen, sondern auch darauf ob Personen
+                    eine Wahl haben, was sie tun wollen, ob die Menschenrechte in ihrem Land eingehalten werden und ob
+                    sie Zugriff zu bestimmten Dienstleistungen (z.B. Arztbesuche) haben.{" "}
                 </p>
             </div>
         ),
         5: (
             <div>
                 <p className="Infotext02">
-                    Wenn die Bürger:innen unsicher werden, produzieren sie weniger Nahrung, was dazu führt, dass es
-                    weniger Essen gibt und die Preise dafür steigen. Deshalb ist es wichtig, dass der Agrarsektor (=
-                    Landwirtschaft) mit Geld unterstützt wird. Dadurch können Hunger und Armut verringert werden, die
-                    Ernährung wird besser gesichert, es entstehen mehr Arbeitsplätze und die Anfälligkeit für
-                    Katastrophen oder andere Bedrohungen sinkt.
+                    An vielen Orten auf dieser Welt werden Menschen schlecht bezahlt oder haben keinen Job und leben
+                    daher unter der Armutsgrenze. Dies schränkt ihren Handlungsspielraum ein und lässt ihnen oft keine
+                    Wahl, sodass sie Berufe mit niedrigen Sicherheitsstandards ausführen müssen, in denen sie möglichen
+                    Gefahren ausgesetzt werden.
                 </p>
             </div>
         ),
         6: (
             <div>
                 <p className="Infotext02">
-                    Eine Welt ohne Hunger kann unser Arbeitsleben, unsere Gesundheit, Bildung, Gleichberechtigung und
-                    gesellschaftliche Entwicklung verbessern. Sie ist sehr wichtig, um eine bessere Zukunft für alle zu
-                    schaffen. Darüber hinaus sind wir mit Hunger nicht in der Lage die anderen Nachhaltigkeitsziele zu
-                    erreichen, da er dazu führt, dass wir uns nicht mehr so gut weiterentwickeln können.{" "}
+                    Wenn wir sicherstellen können, dass jeder einen sicheren und gut bezahlten Job je nach seinen
+                    Fähigkeiten ausführen kann, wird dies natürlich dazu beitragen Armut zu bekämpfen und auszulöschen.
+                    Es gehört aber noch mehr Arbeit dazu. Wir müssen zum Beispiel die Lebensstandards erhöhen, ihnen
+                    Zugriff auf Bildung geben und vieles mehr.{" "}
                 </p>
             </div>
         ),
         7: (
             <div>
                 <p className="Infotext02">
-                    Um Ernährung sicher zu stellen, müssen wir uns mit mehreren Themen befassen: z. B. mit dem Schutz
-                    der Gesellschaft, um sichere und nahrhafte Lebensmittel besonders für Kinder zu gewährleisten. Ein
-                    anderes Beispiel ist die Veränderung der Ernährungssysteme, damit die Welt nachhaltiger und
-                    inklusiver (= mehr alle miteinbeziehen) wird. Du kannst helfen, indem du bei Landwirten oder Märkten
-                    aus deiner Gegend einkaufst und dich für nachhaltige Lebensmittel entscheidest.
-                </p>
-            </div>
-        ),
-        8: (
-            <div>
-                <p className="Infotext02">
-                    Außerdem ist es wichtig, eine gute Ernährung für alle zu unterstützen und Lebensmittelverschwendung
-                    zu bekämpfen. Auch Unternehmen und deine Regierung sollten Entscheidungen und Veränderungen treffen,
-                    die dieses Nachhaltigkeitsziel unterstützen. Bei diesen, sowie in deiner Gemeinde zählt deine Stimme
-                    als zukünftige:r Wähler:in und Verbraucher:in von Nahrung.
+                    Auch wir können gegen die Armut kämpfen indem wir Fördervereinen beitreten, Obdachlosen mit Geld
+                    oder anderen Mitteln helfen oder benutzte Sachen spenden, anstatt sie wegzuwerfen
                 </p>
             </div>
         ),
@@ -116,8 +97,9 @@ export const SDG01_Infotext = (): JSX.Element => {
         2: <p>Warum?</p>,
         3: <p>Was ist das Problem?</p>,
         4: <p>Was ist das Problem?</p>,
-        5: <p>Was ist das Problem?</p>,
-        6: <p>Was können wir tun?</p>,
+        5: <p>Wie ist dieses Thema mit uns verbunden?</p>,
+        6: <p>Wie ist dieses Thema mit uns verbunden?</p>,
+        7: <p> Was können wir tun? </p>,
     };
 
     return (
@@ -126,7 +108,7 @@ export const SDG01_Infotext = (): JSX.Element => {
                 logo={Logo}
                 sdgNumber={sdgNumber}
                 header={header[showtext]}
-                text = {text[showtext]}
+                text={text[showtext]}
                 maxSlides={maxSlides}
                 audioFile={audiofile}
                 eg={EG}
