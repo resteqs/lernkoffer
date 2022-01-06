72da4449954bda8baabba6c392965c8df34d7233
FabiH0903 <91286211+FabiH0903@users.noreply.github.com>
2022-01-06 16:18:28 +0100

    20220601v11 sdg08 gb fertig

---
+++ src/SDG/SDG8/Gute Bsp/SDG08_GB.tsx
@@ -0,0 +1,135 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable unicorn/filename-case */
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable react/react-in-jsx-scope */
+
+import { TextProps } from "../../../Interfaces/SDG";
+import Logo from "../../../SDGLogos/Goal-SDG08.png";
+import Bild05 from "../Gute Bsp/Media/Bild05.png";
+
+export const SDG08_GB: TextProps = {
+    sdgNumber: 8,
+    sdgTitle: "MISEREOR",
+    sdg: "SDG08",
+    logo: Logo,
+    header: {
+        1: " Fakten und Zahlen ",
+        2: " Fakten und Zahlen ",
+        3: "Einsatz vom SOS-Kinderdorf",
+        4: "Einsatz vom SOS-Kinderdorf ",
+        5: "Einsatz vom SOS-Kinderdorf",
+        6: "Aufgabe",
+        7: "Gruppe 1",
+        8: "gruppe 2",
+        9: "Wichtige Dinge für junge Menschen bei der Arbeitssuche",
+        10: "Wichtige Dinge für junge Menschen bei der Arbeitssuche",
+        11: "Gruppe 1 + 2",
+        12: "Unser Beispiel",
+    },
+
+    text: {
+        1: (
+            <div>
+                Jugendarbeitslosigkeit ist eine weltweite Herausforderung: Jugendliche finden keine Arbeit oder bekommen
+                nicht genug Geld zum Überleben. 2018 waren 71 Millionen von ihnen (mit einem Alter von bis zu 24 Jahren)
+                auf der Suche nach Arbeit. Jede:r Fünfte besucht keine Schule, befindet sich in keiner Ausbildung oder
+                hat keine Arbeit. Außerdem machen junge Menschen mehr als ein Drittel aller Arbeitslosen auf der ganzen
+                Welt aus.
+            </div>
+        ),
+        2: (
+            <div>
+                Die Folgen von Jugendarbeitslosigkeit sind Armut, aussichtslose Zukunft, Verzweiflung und Verlust des
+                Selbstbewusstseins. Aus diesem Grund muss man die einzigartigen Talente der jungen Menschen fördern und
+                sie auf den Arbeitsmarkt vorbereiten, damit sie ihre eigene Zukunft und die ihrer Gesellschaft aktiv
+                mitgestalten können.
+            </div>
+        ),
+
+        3: (
+            <div>
+                Die Hilfsorganisation SOS-Kinderdorf hat Jugendförderprogramme eingeführt, die den Jugendlichen
+                Trainings, Berufsberatung und Möglichkeiten zum Kontakte knüpfen mit Partnern aus der Wirtschaft
+                anbieten. Die Ausbildungsprojekte sorgen für eine Bildung passend zu den Interessen, Talenten und dem
+                Bedarf am heimischen Arbeitsmarkt der Jugendlichen.
+            </div>
+        ),
+        4: (
+            <div>
+                Die Beschäftigungs-Initiative „YouthCan!“ (dt.: „Jugend kann“) des SOS-Kinderdorfs arbeitet vor allem
+                mit Jugendlichen, die keine Fürsorge durch ihre Eltern bekommen. Sie hilft ihnen dabei, selbstständig zu
+                werden und die Programme in den SOS-Kinderdörfern wahrzunehmen. Dabei vermitteln sie Arbeitsplätze und
+                Mentor:innen, fördern den Austausch zwischen Jugendlichen und Arbeitgeber:innen und bieten die bereits
+                genannten Projekte an.
+            </div>
+        ),
+        5: (
+            <div>
+                Südafrika war das Ausgangsland von „YouthCan!“, da dort fast die Hälfte aller jungen Menschen arbeitslos
+                ist. Mittlerweile ist dieses Programm weltweit aktiv und besteht aus Expert:innen zur Jugendentwicklung.
+                Das Ziel: „sicherstellen, dass auch Jugendliche mit schwierigem Hintergrund eine positive, wichtige und
+                aktive Rolle in unserer Gesellschaft spielen können“ (Stephen Miller, Direktor von SOS-Kinderdorf
+                Südafrika, 2017).
+            </div>
+        ),
+        6: (
+            <div>
+                Teilt euch nun in zwei Gruppen ein: Gruppe 1 bekommt eine künstlerische Aufgabe, bei der gemalt wird und
+                Gruppe 2 darf sich Gedanken über das Projekt „YouthCan!“ machen und diese aufschreiben.
+            </div>
+        ),
+
+        7: (
+            <div>
+                Überlegt euch jetzt selbst ein Logo für diese Initiative. Dabei könnt ihr zum Beispiel die Schriftart
+                ändern und die Farbe oder etwas um die Buchstaben malen. Wenn alle fertig sind, zeigt euch gegenseitig
+                eure Entwürfe und stimmt für ein Logo ab, welches dann am Ende verwendet werden soll.
+            </div>
+        ),
+        8: (
+            <div>
+                hr habt bisher über ein Projekt gelesen, das Jugendarbeitslosigkeit verhindern und beheben will. Sammelt
+                jetzt Ideen, wie ihr selbst dafür sorgen könnt, dass ihr später einmal Arbeit findet. Jede:r schreibt
+                eigene Tipps auf und anschließend besprecht ihr diese gemeinsam. Seid ihr fertig? Dann klickt weiter und
+                vergleicht mit den Leitgedanken, die mir eingefallen sind.
+            </div>
+        ),
+        9: (
+            <div>
+                <ul>
+                    <li> - bei Berater:innen/Agenturen für Arbeitssuche anmelden und Lernveranstaltungen besuchen</li>
+                    <li> - Hilfe suchen, falls ihr welche braucht</li>
+                    <li> - nicht aufgeben und sich nicht entmutigen lassen nach Absagen</li>
+                    <li> - Kontakte nutze</li>
+                    <li> - selbst aktiv werden und sich bewerben/nachfragen bei Arbeitgeber:innen</li>
+                    <li> - mutig sein und an sich selbst glauben</li>
+                </ul>
+            </div>
+        ),
+        10: (
+            <div> 
+                <ul>
+                    <li> - Praktika oder ehrenamtliche Tätigkeiten zur Orientierung</li>
+                    <li> - sich nach finanzieller (= mit Geld) Unterstützung vom Staat erkundigen</li>
+                    <li> - eigene Stärken und Schwächen erkennen</li>
+                    <li> - Berufswunsch und Interessen herausfinden</li>
+                    <li> - Lebenslauf und letztes Zeugnis bereit haben</li>
+                </ul>
+            </div>
+        ),
+
+        11: (
+            <div>
+                {" "}
+                Wenn beide Gruppen fertig sind, braucht ihr ein großes Blatt Papier als Plakat. Gruppe 1 malt dann
+                beispielsweise in die Mitte ihr eigenes festgelegte Logo von „YouthCan!“ und anschließend schreibt
+                Gruppe 2 ihre Tipps gegen Jugendarbeitslosigkeit um das Logo herum. Danach könnt ihr es aufhängen und
+                als Antrieb für euer späteres Berufsleben sehen.
+            </div>
+        ),
+        12: <div>
+            Das ist nur ein Beispiel Plakat von mir und hier seht ihr mich oben rechts in der Ecke. Wenn ihr wollt, könnt ihr mich auch noch auf euer Plakat malen. Viel Spaß!
+            <img style= {{ marginLeft: "600px",  height: "500px" }} src= {Bild05} alt="" />
+        </div>
+    },
+};
