79dd2d36879b577b5267cf15fd45bab0443f19a5
Daniel Ungar <84130423+Skydreamer-123@users.noreply.github.com>
2022-01-07 14:05:16 +0100

    Revert "v. 20220105 SDG07 Freie Arbeit"

--- src/help.tsx
+++ src/help.tsx
@@ -38,7 +38,7 @@ export const Help = (): JSX.Element => {
                         <td className="tg-c3ow"><Link to ="/SDG04/FreieArbeit">Freie Arbeit</Link></td>
                         <td className="tg-c3ow"><Link to ="/SDG05/FreieArbeit">Freie Arbeit</Link></td>
                         <td className="tg-c3ow"><Link to ="/SDG06/FreieArbeit">Freie Arbeit</Link></td>
-                        <td className="tg-c3ow"><Link to ="/SDG07/FreieArbeit">Freie Arbeit</Link></td>
+                        <td className="tg-c3ow"><Link to ="/SDG07/FreieArbeit">Freie Arbeit WIP</Link></td>
                         <td className="tg-c3ow"><Link to ="/SDG08/FreieArbeit">Freie Arbeit</Link></td>
                         <td className="tg-c3ow"><Link to ="/SDG09/FreieArbeit">Freie Arbeit</Link></td>
                     </tr>
