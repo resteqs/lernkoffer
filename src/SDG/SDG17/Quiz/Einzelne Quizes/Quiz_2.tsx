a210b6ab7a392bf4944f21ffaeac1d4a26aaf32e
misterrabenklaue <79755431+misterrabenklaue@users.noreply.github.com>
2022-01-08 16:11:34 +0100

    finished 17 Quiz_2

--- src/SDG/SDG17/Quiz/Einzelne Quizes/Quiz_2.tsx
+++ src/SDG/SDG17/Quiz/Einzelne Quizes/Quiz_2.tsx
@@ -26,82 +26,93 @@ export const SDG17_Quiz_2: Quiz3Mixed = {
         8: "",
     },
     text: {
-        2:
-            (
-                <div style={{ margin: "auto", display: "block", width: "40%" }}>
-                    <Link to="/SDG17/QuizÜbersicht">
-                        <InfotextButton version="green">Zur Überischt</InfotextButton>
-                    </Link>
-                </div>
-            ),
+        2: (
+            <div style={{ margin: "auto", display: "block", width: "40%" }}>
+                <Link to="/SDG17/QuizÜbersicht">
+                    <InfotextButton version="green">Zur Überischt</InfotextButton>
+                </Link>
+            </div>
+        ),
         4: (
             <div style={{ margin: "auto", display: "block", width: "40%" }}>
-            <Link to="/SDG17/QuizÜbersicht">
-                <InfotextButton version="green">Zur Überischt</InfotextButton>
-            </Link>
-        </div>
+                <Link to="/SDG17/QuizÜbersicht">
+                    <InfotextButton version="green">Zur Überischt</InfotextButton>
+                </Link>
+            </div>
         ),
-        6:                 <div style={{ margin: "auto", display: "block", width: "40%" }}>
-        <Link to="/SDG17/QuizÜbersicht">
-            <InfotextButton version="green">Zur Überischt</InfotextButton>
-        </Link>
-    </div>,
+        6: (
+            <div style={{ margin: "auto", display: "block", width: "40%" }}>
+                <Link to="/SDG17/QuizÜbersicht">
+                    <InfotextButton version="green">Zur Überischt</InfotextButton>
+                </Link>
+            </div>
+        ),
 
         8: (
             <div style={{ margin: "auto", display: "block", width: "40%" }}>
-            <Link to="/SDG17/QuizÜbersicht">
-                <InfotextButton version="green">Zur Überischt</InfotextButton>
-            </Link>
-        </div>
+                <Link to="/SDG17/QuizÜbersicht">
+                    <InfotextButton version="green">Zur Überischt</InfotextButton>
+                </Link>
+            </div>
         ),
     },
     question: {
-        1: "XXX",
-        3: "XXX",
-        5: "XXX",
-        7: "XXX",
+        1: "Wahr oder falsch: Du solltest pro Tag fünf Portionen (ungefähr eine Hand voll) Obst und Gemüse essen",
+        3: "Wenn du Hunger hast, isst du am besten…",
+        5: "Welche Aussage stimmt?",
+        7: "Wo solltest du am besten Essen kaufen?",
     },
     buttonLeftContent: {
-        1: <p>XXX</p>,
-        3: <p>XXX</p>,
-        5: <p>XXX</p>,
-        7: <p>XXX</p>,
+        1: <p style={{ fontSize: "120px" }}>Wahr</p>,
+        3: <p style={{ fontSize: "130px" }}>Brot</p>,
+        5: (
+            <p style={{ fontSize: "45px" }}>
+                Es gibt viel zu wenig Essen auf der Welt, deshalb müssen Menschen in einigen Ländern hungern.{" "}
+            </p>
+        ),
+        7: <p style={{ fontSize: "90px" }}>Super-markt </p>,
     },
     buttonMiddleContent: {
-        1: <p>XXX</p>,
-        3: <p>XXX</p>,
-        5: <p>XXX</p>,
-        7: <p>XXX</p>,
+        3: <p style={{ fontSize: "130px" }}>Obst </p>,
+        5: (
+            <p style={{ fontSize: "50px" }}>
+                Menschen leiden unter Hunger, weil sie so wählerisch bei ihrer Nahrung sind.{" "}
+            </p>
+        ),
+        7: <p style={{ fontSize: "70px" }}>FastFood Restaurant </p>,
     },
     buttonRightContent: {
-        1: <p>XXX</p>,
-        3: <p>XXX</p>,
-        5: <p>XXX</p>,
-        7: <p>XXX</p>,
+        1: <p style={{ fontSize: "120px" }}>Falsch</p>,
+        3: <p style={{ fontSize: "130px" }}>Kekse </p>,
+        5: (
+            <p style={{ fontSize: "47px" }}>
+                Es gibt genug Essen auf der Welt, aber viele Arme können sich davon nichts bzw. nicht genug leisten
+            </p>
+        ),
+        7: <p style={{ fontSize: "90px" }}>Bauern-laden </p>,
     },
     answerNumbersLeft: {
-        1: "XXX",
-        3: "XXX",
-        5: "XXX",
-        7: "XXX",
+        1: "Richtig",
+        3: "Richtig",
+        5: "Falsch",
+        7: "Falsch",
     },
     answerNumbersMiddle: {
-        1: "XXX",
-        3: "XXX",
-        5: "XXX",
-        7: "XXX",
+        3: "Falsch",
+        5: "Falsch",
+        7: "Falsch",
     },
     answerNumbersRight: {
-        1: "XXX",
-        3: "XXX",
-        5: "XXX",
-        7: "XXX",
+        1: "Falsch",
+        3: "Falsch",
+        5: "Richtig",
+        7: "Richtig",
     },
     explanation: {
-        1: "XXX",
-        3: "XXX",
-        5: "XXX",
-        7: "XXX",
+        1: "Am besten sind es drei Portionen Gemüse und 2 Portionen Obst",
+        3: "Brot ist macht lang satt, da es ein Vollkornprodukt ist. Außerdem ist es gesund.",
+        5: "Menschen mit wenig Geld haben meistens nicht die gleichen Möglichkeiten, Essen zu kaufen, wie reiche Menschen. Deshalb mussten ungefähr 750 bis 810 Millionen Menschen hungern im Jahr 2020. Außerdem werden viele Nahrungsmittel weggeschmissen oder für etwas anderes verwendet.",
+        7: "In einem Bauernladen gibt es Lebensmittel aus der Umgebung, sodass es keine langen Transportwege gibt. Das schont die Umwelt. Noch dazu werden so Landwirte aus dei-ner Region unterstützt.",
     },
     // modalContent: <div>Quiz SDG 01</div>,
     isQuizActive: {
@@ -114,7 +125,12 @@ export const SDG17_Quiz_2: Quiz3Mixed = {
         7: true,
         8: false,
     },
-    isThreeAnswerActive: {},
+    isThreeAnswerActive: {
+        1: false,
+        3: true,
+        5: true,
+        7: true,
+    },
     /* eslint-enable unicorn/filename-case */
     /* eslint-enable react/react-in-jsx-scope */
     /* eslint-enable react/no-unescaped-entities */
