038aeabb36ef00695692f7e65824a70826a1fbcd
FabiH0903 <91286211+FabiH0903@users.noreply.github.com>
2022-01-08 13:26:41 +0100

    sdg05 Quiz new layout

--- src/App.tsx
+++ src/App.tsx
@@ -106,7 +106,7 @@ import { SDG04_Spiel } from "./SDG/SDG4/Spiel/04spiel";
 import { SDG05_Einstieg } from "./SDG/SDG5/Einstieg/SDG05_Einstieg";
 import { SDG05_Infotext } from "./SDG/SDG5/Info/SDG05_Infotext";
 /**                                 Quiz                                     */
-import { SDG05_Quiz_Prolog } from "./SDG/SDG5/Quiz/SDG05_Quiz_Prolog";
+
 import { SDG05_Quiz } from "./SDG/SDG5/Quiz/SDG05_Quiz";
 /**                                Spiel                                     */
 import { SDG05_Spiel } from "./SDG/SDG5/Spiel/SDG05_Spiel";
@@ -460,8 +460,7 @@ export const App = (): JSX.Element => {
             <Route exact path="/SDG05/Einstieg"><Quiz_Component sdg = {SDG05_Einstieg}/></Route>
             <Route exact path="/SDG05/Infotext"><InfotextComponent sdg = {SDG05_Infotext}/></Route>
 /**                                 Quiz                                     */
-            <Route exact path ="/SDG05/Quiz/Prolog" component={SDG05_Quiz_Prolog}/>
-            <Route exact path ="/SDG05/Quiz" component={SDG05_Quiz}/>
+<Route exact path="/SDG05/Quiz"><Quiz_Component sdg = {SDG05_Quiz}/></Route>
 /**                                Spiel                                     */
             <Route exact path="/SDG05/Spiel" component={SDG05_Spiel}/>
 /**                                Video                                     */
