0470eb078c56a50f2c0c17d8cecb56936b8bd86f
misterrabenklaue <79755431+misterrabenklaue@users.noreply.github.com>
2022-01-08 18:43:17 +0100

    finished 17 Quiz_9

--- src/SDG/SDG17/Quiz/Einzelne Quizes/Quiz_9.tsx
+++ src/SDG/SDG17/Quiz/Einzelne Quizes/Quiz_9.tsx
@@ -57,52 +57,60 @@ export const SDG17_Quiz_9: Quiz3Mixed = {
         ),
     },
     question: {
-        1: "XXX",
-        3: "XXX",
-        5: "XXX",
-        7: "XXX",
+        1: "Wo begann die erste industrielle Revolution?",
+        3: "Wann wurden diese zwei nachhaltigen Verkehrsmittel erfunden: Fahrrad, Straßenbahn?",
+        5: "Was sollten Firmen vermeiden?",
+        7: "Welche Aussage unterstützt eine nachhaltige Infrastruktur in der Stadt?",
     },
     buttonLeftContent: {
-        1: <p>XXX</p>,
-        3: <p>XXX</p>,
-        5: <p>XXX</p>,
-        7: <p>XXX</p>,
+        1: <p style={{ fontSize: "150px" }}>USA</p>,
+        3: <p style={{ fontSize: "69px" }}>19. Jahrhundert </p>,
+        5: <p style={{ fontSize: "50px" }}>Weniger Fliegen, Müll trennen, zum Trinken Mehrwegbecher benutzen </p>,
+        7: <p style={{ fontSize: "55px" }}>Es sollte keine Plätze mit öffentlichem Internet geben. </p>,
     },
     buttonMiddleContent: {
-        1: <p>XXX</p>,
-        3: <p>XXX</p>,
-        5: <p>XXX</p>,
-        7: <p>XXX</p>,
+        1: <p style={{ fontSize: "80px" }}>Groß-britannien</p>,
+        3: <p style={{ fontSize: "69px" }}>20. Jahrhundert</p>,
+        5: (
+            <p style={{ fontSize: "48px" }}>
+                Recyceltes Papier verwenden, Insektenhotel aufhängen, Computer ausschalten
+            </p>
+        ),
+        7: <p style={{ fontSize: "55px" }}>Jede:r sollte, wenn es geht, sein/ihr Hausdach begrünen</p>,
     },
     buttonRightContent: {
-        1: <p>XXX</p>,
-        3: <p>XXX</p>,
-        5: <p>XXX</p>,
-        7: <p>XXX</p>,
+        1: <p style={{ fontSize: "140px" }}>China</p>,
+        3: <p style={{ fontSize: "69px" }}>21. Jahrhundert</p>,
+        5: (
+            <p style={{ fontSize: "45px" }}>
+                Abfall in der Natur entsorgen, Ausstoß von vielen schädlichen Treibhausgasen, nur neue Produkte kaufen{" "}
+            </p>
+        ),
+        7: <p style={{ fontSize: "55px" }}>Niemand sollte Busse mehrmals pro Woche zu benutzen. </p>,
     },
     answerNumbersLeft: {
-        1: "XXX",
-        3: "XXX",
-        5: "XXX",
-        7: "XXX",
+        1: "Falsch",
+        3: "Richtig",
+        5: "Falsch",
+        7: "Falsch",
     },
     answerNumbersMiddle: {
-        1: "XXX",
-        3: "XXX",
-        5: "XXX",
-        7: "XXX",
+        1: "Richtig",
+        3: "Falsch",
+        5: "Falsch",
+        7: "Richtig",
     },
     answerNumbersRight: {
-        1: "XXX",
-        3: "XXX",
-        5: "XXX",
-        7: "XXX",
+        1: "Falsch",
+        3: "Falsch",
+        5: "Richtig",
+        7: "Falsch",
     },
     explanation: {
-        1: "XXX",
-        3: "XXX",
-        5: "XXX",
-        7: "XXX",
+        1: "In der zweiten Hälfte des 18. Jahrhunderts fing diese bei den Briten durch beispielsweise die Erfindung der Dampfmaschine oder des mechanischen Webstuhls an. Als eine Folge wurden Fabriken gebaut und Produkte nun nicht mehr von Hand gemacht, sondern maschinell in Massenproduktionen hergestellt. Diese industrielle Entwicklung verursacht jedoch bis heute viele Schäden in der Umwelt: verschmutzte Luft, Flüsse und Böden voller Chemikalien und anderer giftiger Stoffe, Waldrodung zur Platzschaffung von Wohnsiedlungen und zum Bau von Eisenbahnstrecken und vieles mehr.",
+        3: "1817 entwickelte Karl Freiherr von Drais die „Laufmaschine“ oder auch „Draisine“. Dieser Vorgänger unseres heutigen Fahrrads hatte noch keine Pedale und ähnelte den Laufrädern, die kleine Kinder heutzutage benutzen. Die erste elektrische Straßenbahn wurde 1881 von Werner von Siemens erfunden. Davor mussten Pferde oder Maultiere die Wagen ziehen. ",
+        5: "Diese Dinge schaden der Umwelt. Stattdessen sollte man Müll trennen und Abfall richtig entsorgen. Firmen sollten außerdem überprüfen, wie viel Treibhausgase sie ausstoßen und diese Menge verringern. Der letzte Punkt „nur neue Produkte kaufen“ sollte ebenfalls vermieden werden. Es ist besser, recycelte Produkte zu kaufen oder zu versuchen, kaputte Dinge reparieren zu lassen.",
+        7: "Eine Dachbegrünung bietet mehrere Vorteile: es sieht gut aus, schützt das Dach, ist ein Lebensraum für Tiere und Pflanzen, erhält die Artenvielfalt, reinigt die Luft, Lärmschutz und vieles mehr. ",
     },
     // modalContent: <div>Quiz SDG 01</div>,
     isQuizActive: {
@@ -115,7 +123,12 @@ export const SDG17_Quiz_9: Quiz3Mixed = {
         7: true,
         8: false,
     },
-    isThreeAnswerActive: {},
+    isThreeAnswerActive: {
+        1: true,
+        3: true,
+        5: true,
+        7: true,
+    },
     /* eslint-enable unicorn/filename-case */
     /* eslint-enable react/react-in-jsx-scope */
     /* eslint-enable react/no-unescaped-entities */
