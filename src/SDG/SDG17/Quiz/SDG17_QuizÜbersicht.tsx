1605b614ce986dfb019ac9c038a69da6e68a10be
misterrabenklaue <79755431+misterrabenklaue@users.noreply.github.com>
2022-01-08 15:49:14 +0100

    Anleitung angepasst

--- src/SDG/SDG17/Quiz/SDG17_QuizÜbersicht.tsx
+++ src/SDG/SDG17/Quiz/SDG17_QuizÜbersicht.tsx
@@ -55,12 +55,13 @@ export const SDG17_Übersicht: TextProps = {
         2: (
             <p>
                 Anschließend fängt eine Person an, würfelt und geht die entsprechende Anzahl an Feldern vor. Wenn ihr
-                auf eines der farbigen Felder kommt, drückt auf dem Tablet auf die entsprechende Nummer des
-                Nachhaltigkeitsziels und euch wird eine Frage zu diesem gestellt. Findet ihr die richtige Antwort, dürft
-                ihr 3 Felder vorrücken, wenn nicht müsst ihr ein Feld zurück. Kommt ihr beim unteren Ende einer Leiter
-                an, dürft ihr sie nach oben klettern zu dem Feld an dem sie endet. Wenn ihr bei dem oberen Ende eines
-                blauen Wasserfalls steht, fällt ihr diesen herunter und müsst zum Feld zurück, an dem er endet. Wer als
-                Erster das Ziel erreicht hat gewonnen.
+                auf eines der farbigen Felder kommt, drückt auf dem Tablet auf das entsprechende Logo des
+                Nachhaltigkeitsziels und euch werden mehrere Fragen dazu getstellt (beantwortet immer nur eine Frage und
+                geht klickt dann auf den Button “Zurück zur Übersicht“, wenn ihr zur zweiten Frage direkt wollt, klickt
+                einfach auf “Weiter“). Findet ihr die richtige Antwort, dürft ihr 3 Felder vorrücken, wenn nicht müsst
+                ihr ein Feld zurück. Kommt ihr beim unteren Ende einer Leiter an, dürft ihr sie nach oben klettern zu
+                dem Feld an dem sie endet. Wenn ihr bei dem oberen Ende eines blauen Wasserfalls steht, fällt ihr diesen
+                herunter und müsst zum Feld zurück, an dem er endet. Wer als Erster das Ziel erreicht hat gewonnen.
             </p>
         ),
         3: (
@@ -78,10 +79,12 @@ export const SDG17_Übersicht: TextProps = {
                 <LogoButton URL="/SDG17/Quiz11" Logo={Logo11} />
                 <LogoButton URL="/SDG17/Quiz12" Logo={Logo12} />
                 <LogoButton URL="/SDG17/Quiz13" Logo={Logo13} />
-                <LogoButton URL="/SDG17/Quiz14" Logo={Logo14} />    
-                <div style={{ marginLeft: "520px" }}><LogoButton URL="/SDG17/Quiz15" Logo={Logo15}/></div>
-                <LogoButton URL="/SDG17/Quiz16" Logo={Logo16}/>
-                <LogoButton URL="/SDG17/Quiz17" Logo={Logo17}/>
+                <LogoButton URL="/SDG17/Quiz14" Logo={Logo14} />
+                <div style={{ marginLeft: "520px" }}>
+                    <LogoButton URL="/SDG17/Quiz15" Logo={Logo15} />
+                </div>
+                <LogoButton URL="/SDG17/Quiz16" Logo={Logo16} />
+                <LogoButton URL="/SDG17/Quiz17" Logo={Logo17} />
             </div>
         ),
     },
