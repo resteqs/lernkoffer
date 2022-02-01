d24e0fe50edca386fb63afa200c1c7618547f8d0
Antoni 'resteqs' Mackowiak <65457699+resteqs@users.noreply.github.com>
2022-02-01 14:29:53 +0100

    SDG 11 fertig bis auf den letzten Punkt

--- src/SDG/SDG11/Spiel/SDG11_Spiel.tsx
+++ src/SDG/SDG11/Spiel/SDG11_Spiel.tsx
@@ -26,16 +26,16 @@ export const SDG11_Spiel: TextProps = {
 
     text: {
         1: (
-            "Der ökologische Fußabdruck eines Menschen zeigt, wie umweltfreundlich jemand lebt. Hierbei werden verschiedene Entscheidungen, die du triffst, bewertet."
+            "Der ökologische Fußabdruck zeigt auf, wie umweltbewusst ein Mensch lebt. Hierbei werden verschiedene Entscheidungen, die du triffst, bewertet."
         ),
         2: (
             <ul>
                 <li>
                     {" "}
-                    Im Folgenden wirst du immer eine Frage bekommen und drei verschiedene Antwortmöglichkeiten. Wenn
+                    Im Folgenden werden die Entscheidungnen, die du triffst, nach ihrer Umweltverträglichkeit bewertet. Wenn
                     du die Frage und die Antworten gelesen hast, nimm dir kurz Zeit und überlege, welche der
                     Antworten am besten auf dich zutrifft. Dahinter siehst du in Klammern immer eine Zahl. Das sind
-                    die Punkte, die du sammelst. Diese Punkte zählst du während dem Spielen zusammen.{" "}
+                    die Punkte, die du sammelst. Diese Punkte zählst du während des Spiels zusammen.{" "}
                 </li>
                 <li>
                     {" "}
@@ -73,7 +73,7 @@ export const SDG11_Spiel: TextProps = {
         ),
         5: (
             <div>
-                <p> Was machst du mit Kleidung und Spielzeuge, die du nicht mehr benötigst? </p>
+                <p> Was machst du mit Kleidung und Spielzeug, welche du nicht mehr benötigst? </p>
                 <ul className="flex-container ">
                     <li className="flex-item"> Spenden (0) </li>
                     <li className="flex-item"> Wegwerfen (2) </li>
@@ -85,9 +85,9 @@ export const SDG11_Spiel: TextProps = {
             <div>
                 <p> Wie oft isst du Fleisch in der Woche?</p>
                 <ul className="flex-container ">
-                    <li className="flex-item"> Kein bis ein Mal (0) </li>
-                    <li className="flex-item"> Zwei bis drei mal (1) </li>
-                    <li className="flex-item"> Vier oder öfter (2) </li>
+                    <li className="flex-item"> Nie bis ein Mal (0) </li>
+                    <li className="flex-item"> Zwei- bis dreimal (1) </li>
+                    <li className="flex-item"> Viermal oder öfter (2) </li>
                 </ul>
             </div>
         ),
