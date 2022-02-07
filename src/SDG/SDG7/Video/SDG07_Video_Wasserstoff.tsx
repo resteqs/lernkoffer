b4b332745108ceedf8e16e1c058c8517292ba51e
FabiH0903 <91286211+FabiH0903@users.noreply.github.com>
2022-02-07 21:00:02 +0100

    Updates ich habe keine lust mehr

---
+++ src/SDG/SDG7/Video/SDG07_Video_Wasserstoff.tsx
@@ -0,0 +1,17 @@
+
+// eslint-disable-next-line unicorn/filename-case
+import * as React from "react"
+import { VideoProps } from "../../../Interfaces/SDG";
+import Logo from "../../../SDGLogos/Goal-SDG07.png";
+import Wasserstoff from "./Wasserstoff.mp4";
+
+
+export const SDG07_Video_Wasserstoff:VideoProps = {
+    sdgNumber: 7,
+    sdgTitle: "Kurzerklärt: Wasserstoff Energie ",
+    sdg: "SDG07",
+    logo: Logo,
+    source: Wasserstoff,
+    quelle:<p> <a href="https://www.tagesschau.de/multimedia/video/video-cc-199~_parentId-creativecommonsdossier-101.html">„Kurzerklärt: Wasserstoff Energie </a> by „Tagesschau“ is licensed under <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/deed.de"> „CC BY-NC-ND 4.0“ </a> <a href="https://www.tagesschau.de/multimedia/video/video-cc-123~_parentId-creativecommonsdossier-101.html"> (Angaben der Tagesschau)  </a>  </p>,
+    isCCBY: false, 	
+}
