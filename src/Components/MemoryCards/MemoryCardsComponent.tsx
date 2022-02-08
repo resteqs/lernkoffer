37ba418cd5b0f09875aff684a8075c7eb3e95ce9
Skydreamer-123 <danielungar1@gmx.de>
2022-02-08 21:04:53 +0100

    changing toggling SDG07 Spiel

--- src/Components/MemoryCards/MemoryCardsComponent.tsx
+++ src/Components/MemoryCards/MemoryCardsComponent.tsx
@@ -1,28 +1,37 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 
-import * as React from 'react';
+import * as React from "react";
 
 export interface CardProps {
-    id:string,
-    image:string,
-    front:string,
-    solved?:boolean,
-    flipped?: boolean
+    id: string;
+    image: string;
+    front: string;
+    solved?: boolean;
+    flipped?: boolean;
 }
 
-export const MemoryCardsComponent = (props:CardProps):JSX.Element => {
-    const [isflipped, setIsflipped] = React.useState(false)
-    const [issolved, setIsSolved] = React.useState(false)
-    const [isFrontImage, setFrontImage] = React.useState("")
-    const cardImage = document.getElementById("cardImage")
+export const MemoryCardsComponent = (props: CardProps): JSX.Element => {
+    const [isflipped, setIsflipped] = React.useState(false);
+    const [issolved, setIsSolved] = React.useState(false);
+    const [isFrontImage, setFrontImage] = React.useState("");
+    const cardImage = document.getElementById("cardImage");
 
-    // , width:"100vw", margin:"auto", marginTop:"0px",  , justifyItems:"baseline" 
-    return(
-        <div style={{ overflow: "hidden", clear: "both", display:"inline-block" }}>
-            <button  style={{ width: "15vw", margin:"10px" }} type='button' onClick={()=>setIsflipped(!isflipped)}>
-                {isflipped === false? <img  style={{ height: "auto", width:"100%" }} src= {props.front} alt="" />: <img style= {{ width: "100%" }} src= {props.image} alt="" />}
-                
-            </button>
+    const toggleChild = (): void => {
+        setIsflipped(!isflipped);
+    };
+    // , width:"100vw", margin:"auto", marginTop:"0px",  , justifyItems:"baseline"
+    return (
+        <div style={{ overflow: "hidden", clear: "both", display: "inline-block", margin:"2vh" }}>
+            <img
+                src={isflipped ? props.image : props.front}
+                alt=""
+                typeof="button"
+                onClick={() => toggleChild()}
+                style={{ height: "auto", width: "15vw", maxHeight:"15vw" }}
+            />
         </div>
     );
-    
-}
+};
