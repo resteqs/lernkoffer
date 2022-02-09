c9cd510eb8be1fcf797bc5fde906bafc3412f58a
FabiH0903 <91286211+FabiH0903@users.noreply.github.com>
2022-02-09 16:46:34 +0100

    quellen

---
+++ src/SDG/SDG11/Video/SDG11_Video.tsx
@@ -0,0 +1,16 @@
+
+// eslint-disable-next-line unicorn/filename-case
+import * as React from "react"
+import { VideoProps } from "../../../Interfaces/SDG";
+import Logo from "../../../SDGLogos/Goal-SDG11.png";
+import erste_Grosssiedlung_der_Menschheit  from "./erste_Grosssiedlung_der_Menschheit.mp4";
+
+
+export const SDG11_Video:VideoProps = {
+    sdgNumber: 11,
+    sdgTitle: "Erste Großsiedlung der Menschheit",
+    sdg: "SDG11",
+    logo: Logo,
+    source: erste_Grosssiedlung_der_Menschheit ,
+    quelle:<p>  <a href="https://www.zdf.de/dokumentation/terra-x/catalhoeyuek-erste-grosssiedlung-der-menschheit-creative-commons-100.html"> „Catalhöyük: erste Großsiedlung der Menschheit“ </a>by ZDF/Terra X/Film Produktion Stein e.K./Alexander Hogh/Martin Papirowski/Timm Westen, Roxana Ardelean/Golem Studio/Alexander Leuck/Frauenhofer Intitut für Graphische Datenverarbeitung/Christofori u.Partner is licensed by <a href="https://creativecommons.org/licenses/by/4.0/legalcode.de"> „CC BY 4.0“ </a><a href="https://www.zdf.de/dokumentation/terra-x/terra-x-creative-commons-cc-100.html">(Angaben von Terra X)</a> </p>
+}
---
+++ src/SDG/SDG17/Video/SDG17_Video.tsx
@@ -0,0 +1,16 @@
+
+// eslint-disable-next-line unicorn/filename-case
+import * as React from "react"
+import { VideoProps } from "../../../Interfaces/SDG";
+import Logo from "../../../SDGLogos/Goal-SDG17.png";
+import Nachhaltigkeit_Definition_allgemein  from "./Nachhaltigkeit_Definition_allgemein.mp4";
+
+export const SDG17_Video:VideoProps = {
+    sdgNumber: 17,
+    sdgTitle: "Nachhaltigkeit Definition",
+    sdg: "SDG17",
+    logo: Logo,
+    source: Nachhaltigkeit_Definition_allgemein,
+    quelle:<p>  <a href="https://www.zdf.de/dokumentation/terra-x/was-bedeutet-nachhaltigkeit-creative-commons-clip-100.html"> „Was bedeutet Nachhaltigkeit?“</a>by ZDF/logo/Karola Körber  is licensed under <a href="https://creativecommons.org/licenses/by/4.0/legalcode.de"> „CC BY 4.0“ </a><a href="https://www.zdf.de/dokumentation/terra-x/terra-x-creative-commons-cc-100.html">(Angaben von Terra X)</a> </p>
+}
+
